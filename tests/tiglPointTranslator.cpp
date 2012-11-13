8f874a237d393798ebcc93dbb7bad9cb35737324
Martin Siggel <martin.siggel@dlr.de>
2012-11-13 21:25:16 +0100

    added more unit tests for tiglPointTranslator, also added Performance test

--- tests/tiglPointTranslator.cpp
+++ tests/tiglPointTranslator.cpp
@@ -2,9 +2,6 @@
 * Copyright (C) 2007-2011 German Aerospace Center (DLR/SC)
 *
 * Created: 2012-11-13 Martin Siggel <Martin.Siggel@dlr.de>
-* Changed: $Id$ 
-*
-* Version: $Revision$
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -26,6 +23,8 @@
 #include "test.h"
 #include "tigl.h"
 #include "CTiglPointTranslator.h"
+#include <ctime>
+#include <cstdio>
 
 using namespace tigl;
 
@@ -42,24 +41,61 @@ TEST(TiglPointTranslator, simple){
     CTiglPointTranslator trans(x1, x2, x3, x4 );
 
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.5, eta, 1e-5);
-    ASSERT_NEAR( 0.5, xsi, 1e-5);
+    ASSERT_NEAR( 0.5, eta, 1e-7);
+    ASSERT_NEAR( 0.5, xsi, 1e-7);
 
     p.x = 1; p.y = 0; p.z = 0;
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.5, eta, 1e-5);
-    ASSERT_NEAR( 1.0, xsi, 1e-5);
+    ASSERT_NEAR( 0.5, eta, 1e-7);
+    ASSERT_NEAR( 1.0, xsi, 1e-7);
 
     p.x = 0; p.y = 0; p.z = 0;
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.0, eta, 1e-5);
-    ASSERT_NEAR( 1.0, xsi, 1e-5);
+    ASSERT_NEAR( 0.0, eta, 1e-7);
+    ASSERT_NEAR( 1.0, xsi, 1e-7);
 
     p.x = 1.5; p.y = 1; p.z = 3;
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.75, eta, 1e-5);
-    ASSERT_NEAR( 0.5 , xsi, 1e-5);
+    ASSERT_NEAR( 0.75, eta, 1e-7);
+    ASSERT_NEAR( 0.5 , xsi, 1e-7);
 
+}
+
+TEST(TiglPointTranslator, simple2){
+    CTiglPoint x1(-1,0,0);
+    CTiglPoint x2(0,1,0);
+    CTiglPoint x3(0,-1,0);
+    CTiglPoint x4(1,0,0);
+
+    CTiglPoint p(0,0,0);
+
+    double eta, xsi;
+
+    CTiglPointTranslator trans(x1, x2, x3, x4 );
+
+    ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
+    ASSERT_NEAR( 0.5, eta, 1e-7);
+    ASSERT_NEAR( 0.5, xsi, 1e-7);
+    
+    p.x = -1.; p.y = 0.; p.z = 0;
+    ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
+    ASSERT_NEAR( 0., eta, 1e-7);
+    ASSERT_NEAR( 0., xsi, 1e-7);
+    
+    p.x = 0.; p.y = 1.; p.z = 0;
+    ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
+    ASSERT_NEAR( 1., eta, 1e-7);
+    ASSERT_NEAR( 0., xsi, 1e-7);
+    
+    p.x = 0.; p.y = -1.; p.z = 0;
+    ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
+    ASSERT_NEAR( 0., eta, 1e-7);
+    ASSERT_NEAR( 1., xsi, 1e-7);
+    
+    p.x = 1.; p.y = 0.; p.z = 0;
+    ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
+    ASSERT_NEAR( 1., eta, 1e-7);
+    ASSERT_NEAR( 1., xsi, 1e-7);            
 }
 
 TEST(TiglPointTranslator, advanced){
@@ -75,16 +111,52 @@ TEST(TiglPointTranslator, advanced){
     CTiglPointTranslator trans(x1, x2, x3, x4 );
 
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.5, eta, 1e-5);
-    ASSERT_NEAR( 0.5, xsi, 1e-5);
+    ASSERT_NEAR( 0.5, eta, 1e-7);
+    ASSERT_NEAR( 0.5, xsi, 1e-7);
 
     p.x = 1.5; p.y = 2; p.z = 0;
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.25, eta, 1e-5);
-    ASSERT_NEAR( 0.5 , xsi, 1e-5);
+    ASSERT_NEAR( 0.25, eta, 1e-7);
+    ASSERT_NEAR( 0.5 , xsi, 1e-7);
 
     p.x = 1.5; p.y = 2; p.z = -3;
     ASSERT_EQ ( TIGL_SUCCESS,  trans.translate(p, &eta, &xsi) );
-    ASSERT_NEAR( 0.25, eta, 1e-5);
-    ASSERT_NEAR( 0.5 , xsi, 1e-5);
+    ASSERT_NEAR( 0.25, eta, 1e-7);
+    ASSERT_NEAR( 0.5 , xsi, 1e-7);
+}
+
+TEST(TiglPointTranslator, performance){
+    int nruns = 1000000;
+    
+    CTiglPoint x1(0,4,0);
+    CTiglPoint x2(8,4,0);
+    CTiglPoint x3(0,0,0);
+    CTiglPoint x4(4,0,0);
+
+    CTiglPoint p(3,2,1);
+
+    double eta, xsi;
+    double x = 0.;
+
+    CTiglPointTranslator trans(x1, x2, x3, x4 );
+
+	clock_t start = clock();
+
+    for(int i = 0; i < nruns; ++i){
+        trans.translate(p, &eta, &xsi) ;
+        //just some dummy to prevent compiler optimization
+        x = x + 1.0;
+	}
+	
+	ASSERT_EQ((double)nruns, x);
+	ASSERT_NEAR(0.5, eta, 1e-7);
+	ASSERT_NEAR(0.5, xsi, 1e-7);
+        
+    clock_t stop = clock();
+    double time_elapsed = (double)(stop - start)/(double)CLOCKS_PER_SEC/(double)nruns;
+    time_elapsed *= 1000000.;
+    printf("Elapsed average time: %f [us]\n", time_elapsed);
+    
+    ASSERT_TRUE(true);
+   
 }
