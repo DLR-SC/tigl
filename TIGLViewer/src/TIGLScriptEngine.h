1006d970f5303d01edf72d59c2593a9eccda8ea7
Markus Litz <markus.litz@dlr.de>
2012-11-21 10:14:35 +0100

    Added action to open a script-file from within tiglviewer.

--- TIGLViewer/src/TIGLScriptEngine.h
+++ TIGLViewer/src/TIGLScriptEngine.h
@@ -10,7 +10,7 @@
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-* Â  Â  http://www.apache.org/licenses/LICENSE-2.0
+* ï¿½  ï¿½  http://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,6 +36,7 @@ class TIGLScriptEngine :public QObject
         public:
                 TIGLScriptEngine();
                 //~TIGLScriptEngine();
+                void openFile(QString fileName);
         
         public slots:
                 void textChanged(QString line);
