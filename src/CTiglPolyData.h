eddbfcaa1ca28bb6999b5df999a6132d0bb24df2
Martin Siggel <martin.siggel@dlr.de>
2013-02-09 16:54:33 +0100

    exchanged polypoint class with CTiglPoint class

--- src/CTiglPolyData.h
+++ src/CTiglPolyData.h
@@ -1,43 +1,48 @@
+/*
+* Copyright (C) 2007-2013 German Aerospace Center (DLR/SC)
+*
+* Created: 2013-02-09 Martin Siggel <Martin.Siggel@dlr.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @file CTiglPolyData.h
+* @brief Handles polygon data for export and rendering usage
+*/
+
 #ifndef VTKPOLYDATA_H
 #define VTKPOLYDATA_H
 
 #include <ostream>
 
-class PolyPoint{
-public:
-    PolyPoint(){
-        x = 0.; y = 0.; z = 0.;
-    }
-    
-    PolyPoint(double X,double Y,double Z){
-        x = X; y = Y; z = Z;
-    }
-    
-    // returns sqare distance to another point
-    double dist2(const PolyPoint& p) const {
-        return (x-p.x)*(x-p.x) + (y-p.y)*(y-p.y) + (z-p.z)*(z-p.z);
-    }
-    
-    void print()const;
-    
-    void getMinMax(double & min, double & max) const;
-    
-    double x,y,z;
-};
+class PolyDataImpl;
 
+namespace tigl {
 
 class CTiglPolyData{
 public:
     CTiglPolyData();
     ~CTiglPolyData();
     
-    void addPoint(const PolyPoint& p, int polynum);
+    // adds a point to a polygon with a specific id
+    void addPoint(const class CTiglPoint& p, int id);
     
     void writeVTK(const char * filename);
     void printVTK();
     
 private:
-    class PolyDataImpl * impl;
+    PolyDataImpl * impl;
 };
+
+}
 
 #endif // VTKPOLYDATA_H
