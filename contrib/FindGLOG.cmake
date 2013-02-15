3c2171b5e573355ebcc40d01e3b8f25c05d5bc93
Markus Litz <markus.litz@dlr.de>
2013-02-15 21:36:34 +0100

    Removed glog-sources from project.

--- contrib/FindGLOG.cmake
+++ contrib/FindGLOG.cmake
@@ -12,24 +12,6 @@ FIND_PATH(GLOG_INCLUDE_DIR glog/logging.h
   /usr/local/include
   /usr/include
   )
-  
-message(STATUS "Glog first pass: ${GLOG_INCLUDE_DIR}")
-if (NOT GLOG_INCLUDE_DIR )
-  message(STATUS "Building glog in ${CMAKE_BINARY_DIR}/thirdparty/glog...")
-  execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/thirdparty/glog)
-  execute_process(
-    COMMAND ${PROJECT_SOURCE_DIR}/thirdparty/glog-0.3.3/configure --prefix=${CMAKE_BINARY_DIR}/thirdparty/glog
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/thirdparty/glog-0.3.3
-    OUTPUT_QUIET
-    OUTPUT_FILE "glog"
-    )
-  message(STATUS "Installing glog in ${CMAKE_BINARY_DIR}/thirdparty/glog...")
-  execute_process(
-    COMMAND make install
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/thirdparty/glog-0.3.3
-    OUTPUT_QUIET
-    )
-endif()
 
 FIND_LIBRARY(GLOG_LIBRARY
   NAMES glog
@@ -39,15 +21,9 @@ FIND_LIBRARY(GLOG_LIBRARY
   /usr/local/lib
   /usr/lib
   )
-FIND_PATH(GLOG_INCLUDE_DIR glog/logging.h
-  ${CMAKE_BINARY_DIR}/thirdparty/glog/include
-  /opt/local/include
-  /usr/local/include
-  /usr/include
-  )
   
 set(GLOG_LIBRARIES ${GLOG_LIBRARY})
-set(GLOG_INCLUDE_DIR ${CMAKE_BINARY_DIR}/thirdparty/glog/include)
+set(GLOG_INCLUDE_DIR ${GLOG_INCLUDE_DIR})
 message(STATUS "GLog includes: ${GLOG_INCLUDE_DIR} Libraries: ${GLOG_LIBRARIES}" )
 
 # handle the QUIETLY and REQUIRED arguments and set OpenTURNS_FOUND to TRUE if
