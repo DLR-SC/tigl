5ad25456b3a40a7be998ddd4a428b342cf40cd36
Martin Siggel <martin.siggel@dlr.de>
2013-02-18 14:07:05 +0100

    Fixed google::log in windows (hopefully)

--- contrib/FindGLOG.cmake
+++ contrib/FindGLOG.cmake
@@ -6,29 +6,28 @@
 
 # try to find glog headers, if not found then install glog from contrib into
 # build directory and set GLOG_INCLUDE_DIR and GLOG_LIBRARIES
-FIND_PATH(GLOG_INCLUDE_DIR glog/logging.h
-  ${CMAKE_BINARY_DIR}/thirdparty/glog/include
+FIND_PATH(GLOG_INCLUDE_DIR 
+  NAMES glog/logging.h
+  PATHS
   /opt/local/include
-  /usr/local/include
-  /usr/include
-  )
+)
 
 FIND_LIBRARY(GLOG_LIBRARY
-  NAMES glog
+  NAMES glog 
+  # and for windows
+  libglog
   PATHS
-  ${CMAKE_BINARY_DIR}/thirdparty/glog/lib/
   /opt/local/lib
   /usr/local/lib
   /usr/lib
   )
-  
-set(GLOG_LIBRARIES ${GLOG_LIBRARY})
-set(GLOG_INCLUDE_DIR ${GLOG_INCLUDE_DIR})
-message(STATUS "GLog includes: ${GLOG_INCLUDE_DIR} Libraries: ${GLOG_LIBRARIES}" )
 
-# handle the QUIETLY and REQUIRED arguments and set OpenTURNS_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set GLOG_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (GLOG DEFAULT_MSG GLOG_INCLUDE_DIR GLOG_LIBRARIES )
+find_package_handle_standard_args (GLOG REQUIRED_VARS GLOG_INCLUDE_DIR GLOG_LIBRARY )
 
-mark_as_advanced (GLOG_INCLUDE_DIR GLOG_LIBRARIES)
+IF(GLOG_FOUND)
+  SET(GLOG_LIBRARIES ${GLOG_LIBRARY})
+ENDIF(GLOG_FOUND)
+mark_as_advanced (GLOG_INCLUDE_DIR GLOG_LIBRARY)
