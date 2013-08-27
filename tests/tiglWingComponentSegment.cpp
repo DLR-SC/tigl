263cf28e3ad69328116ebb724f9df21f7dd042ef
Martin Siggel <martin.siggel@dlr.de>
2013-08-27 15:01:04 +0200

    Reenabled test that was always failing. Now it doesn't anymore :)

--- tests/tiglWingComponentSegment.cpp
+++ tests/tiglWingComponentSegment.cpp
@@ -584,14 +584,16 @@ TEST(WingComponentSegment4, tiglWingComponentSegmentPointGetSegmentEtaXsi_BUG2){
     tixiCloseDocument(tixiHandle);
 }
 
-TEST_F(WingComponentSegment3, DISABLED_tiglWingComponentSegmentPointGetSegmentEtaXsi_BUG3){
+TEST_F(WingComponentSegment3, tiglWingComponentSegmentPointGetSegmentEtaXsi_BUG3){
     double sEta = 0., sXsi = 0.;
     char *wingUID = NULL, *segmentUID = NULL;
     TiglReturnCode ret = tiglWingComponentSegmentPointGetSegmentEtaXsi(tiglHandle, "D150_VTP_CS", 0.0, 1.0, &wingUID, &segmentUID, &sEta, &sXsi);
     ASSERT_EQ(TIGL_SUCCESS, ret);
-    //ASSERT_STREQ("D150_wing_1ID", wingUID);
-    //ASSERT_STREQ("D150_wing_1Segment2ID", segmentUID);
-    cout << "eta_s / xsi_s: " << sEta << "/" << sXsi << endl;
+    ASSERT_STREQ("D150_VTP_1ID", wingUID);
+    ASSERT_STREQ("D150_VTP_1Segment2ID", segmentUID);
+
+    ASSERT_NEAR(0.0, sEta, 0.0001);
+    ASSERT_NEAR(1.0, sXsi, 0.0001);
 }
 
 TEST_F(WingComponentSegment, tiglWingComponentSegmentPointGetSegmentEtaXsi_BUG4){
