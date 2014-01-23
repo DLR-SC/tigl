32abd4a7a8605e169de8135c0d097b051fc63722
Martin Siggel <martin.siggel@dlr.de>
2014-01-23 23:35:09 +0100

    Converted Hotsose mesh import to boost strings instead of qt strings
    Added some more boost files to support string operations.

--- TIGLViewer/src/CHotsoseMeshReader.cpp
+++ TIGLViewer/src/CHotsoseMeshReader.cpp
@@ -18,10 +18,13 @@
 
 
 #include "CHotsoseMeshReader.h"
-#include <QFile>
-#include <QTextStream>
-#include <QString>
-#include <QStringList>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <fstream>
 
 #include "CTiglLogging.h"
 
@@ -29,30 +32,31 @@ CHotsoseMeshReader::CHotsoseMeshReader()
 {
 }
 
-TiglReturnCode CHotsoseMeshReader::readFromFile(const char * filename, tigl::CTiglPolyData& mesh){
+TiglReturnCode CHotsoseMeshReader::readFromFile(const char * filename, tigl::CTiglPolyData& mesh)
+{
+    using namespace boost::algorithm;
     mesh.currentObject().enableNormals(false);
 
-    QFile file(filename);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+    std::ifstream file(filename);
+    if (!file.is_open()) {
         LOG(ERROR) << "Cannot read file " << filename;
         return TIGL_OPEN_FAILED;
     }
 
-    QTextStream in(&file);
-    QString line = in.readLine();
-
+    std::string line;
     int linenr = 0;
     int lastnr = 0;
     int polycount = 0;
     tigl::CTiglPolygon* poly = new tigl::CTiglPolygon;
-    while (!line.isNull()) {
-        line = in.readLine();
+    while (std::getline(file, line)) {
 
-        QStringList list = line.split(" ", QString::SkipEmptyParts);
-        if(list.size() == 4){
+        std::vector<std::string> list;
+        trim(line);
+        split(list, line, boost::is_any_of("\t "), token_compress_on);
+        if (list.size() == 4){
             // parse polygon number
-            int pnr = list.at(0).toInt();
-            if (pnr != lastnr && poly->getNPoints() > 0){
+            int pnr = boost::lexical_cast<int>(list.at(0));
+            if (pnr != lastnr && poly->getNPoints() > 0) {
                 mesh.currentObject().addPolygon(*poly);
                 // make a mirrored copy of the poly
                 tigl::CTiglPolygon polymirr;
@@ -71,22 +75,25 @@ TiglReturnCode CHotsoseMeshReader::readFromFile(const char * filename, tigl::CTi
             lastnr = pnr;
 
             tigl::CTiglPoint p;
-            QString sx = list.at(1);
-            QString sy = list.at(2);
-            QString sz = list.at(3);
+            std::string sx = list.at(1);
+            std::string sy = list.at(2);
+            std::string sz = list.at(3);
 
             // gghs number output might be something like 3.4100D-01
             // convert it into standard form (with E)
-            p.x = sx.replace("D", "E").toDouble();
-            p.y = sy.replace("D", "E").toDouble();
-            p.z = sz.replace("D", "E").toDouble();
+            replace_all(sx, "D", "E");
+            replace_all(sy, "D", "E");
+            replace_all(sz, "D", "E");
+            p.x = boost::lexical_cast<double>(sx);
+            p.y = boost::lexical_cast<double>(sy);
+            p.z = boost::lexical_cast<double>(sz);
 
             poly->addPoint(p);
         }
 
         linenr++;
     }
-    if (poly && poly->getNPoints() > 0){
+    if (poly && poly->getNPoints() > 0) {
         mesh.currentObject().addPolygon(*poly);
         tigl::CTiglPolygon polymirr;
         for (int i = poly->getNPoints()-1; i >= 0; --i) {
