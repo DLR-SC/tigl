11942b89c9f9ccdf24b55c47b5e68287a6097faa
Martin Siggel <martin.siggel@dlr.de>
2014-02-19 15:11:38 +0100

    fixed checkstyle scripts

--- contrib/tiglmacros.cmake
+++ contrib/tiglmacros.cmake
@@ -4,6 +4,7 @@ function(AddToCheckstyle)
 
     add_custom_command(
         OUTPUT ${dir}_checkstyle-report
+        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/filefilter.txt
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
         COMMAND ${PYTHON_EXECUTABLE}
         ARGS ${PROJECT_SOURCE_DIR}/thirdparty/nsiqcppstyle/nsiqcppstyle.py ${CMAKE_CURRENT_SOURCE_DIR}
@@ -12,17 +13,17 @@ function(AddToCheckstyle)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/filefilter.txt
         COMMAND ${CMAKE_COMMAND}
-        ARGS -E copy ${PROJECT_SOURCE_DIR}/contrib/filefilter.txt ${CMAKE_CURRENT_SOURCE_DIR}
+        ARGS -E copy ${PROJECT_SOURCE_DIR}/contrib/filefilter.txt ${CMAKE_CURRENT_SOURCE_DIR}/
     )
 
     add_custom_target(
         checkstyle_${dir}
         DEPENDS ${dir}_checkstyle-report
-        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/filefilter.txt
+
     )
 
 
     add_dependencies(checkstyle checkstyle_${dir})
 
-  endif(PYTHONINTTERP_FOUND)
+  endif(PYTHONINTERP_FOUND)
 endfunction()
