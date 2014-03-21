a70c7ba00112d806281b37345bbb98ef816ff054
Martin Siggel <martin.siggel@dlr.de>
2014-03-21 10:53:18 +0100

    Checkstyle fixes

--- TIGLViewer/src/TIGLViewerShapeIntersectionDialog.cpp
+++ TIGLViewer/src/TIGLViewerShapeIntersectionDialog.cpp
@@ -1,3 +1,21 @@
+/*
+* Copyright (C) 2007-2013 German Aerospace Center (DLR/SC)
+*
+* Created: 2014-03-18 Martin Siggel <martin.siggel@dlr.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include "TIGLViewerShapeIntersectionDialog.h"
 #include "ui_TIGLViewerShapeIntersectionDialog.h"
 
@@ -18,12 +36,12 @@ TIGLViewerShapeIntersectionDialog::TIGLViewerShapeIntersectionDialog(tigl::CTigl
     uidManager(m)
 {
     ui->setupUi(this);
-    
+
     // fill combo boxes
     const tigl::ShapeContainerType& uids = m.GetShapeContainer();
     tigl::ShapeContainerType::const_iterator it;
     std::vector<std::string> uidStrings;
-    
+
     for (it = uids.begin(); it != uids.end(); ++it) {
         uidStrings.push_back(it->first);
     }
@@ -37,7 +55,7 @@ TIGLViewerShapeIntersectionDialog::TIGLViewerShapeIntersectionDialog(tigl::CTigl
         ui->shape2Combo->addItem(uid.c_str());
         ui->shapeCombo->addItem(uid.c_str());
     }
-    
+
     // set point and normal
     ui->pxSB->setValue(p.x);
     ui->pySB->setValue(p.y);
@@ -45,8 +63,8 @@ TIGLViewerShapeIntersectionDialog::TIGLViewerShapeIntersectionDialog(tigl::CTigl
     ui->nxSB->setValue(n.x);
     ui->nySB->setValue(n.y);
     ui->nzSB->setValue(n.z);
-    
     // set selected comboboxes
+    
     if (shape1Selected >= 0) {
         ui->shape1Combo->setCurrentIndex(shape1Selected);
     }
@@ -56,10 +74,10 @@ TIGLViewerShapeIntersectionDialog::TIGLViewerShapeIntersectionDialog(tigl::CTigl
     if (shapeSelected >= 0) {
         ui->shapeCombo->setCurrentIndex(shapeSelected);
     }
-    
+
     // select tab
     ui->TabView->setCurrentIndex(lastMode);
-    
+
     // connect signals
     connect(ui->pxSB, SIGNAL(valueChanged(double)), this, SLOT(OnItemChanged()));
     connect(ui->pySB, SIGNAL(valueChanged(double)), this, SLOT(OnItemChanged()));
@@ -78,15 +96,15 @@ void TIGLViewerShapeIntersectionDialog::OnItemChanged()
     p.x = ui->pxSB->value();
     p.y = ui->pySB->value();
     p.z = ui->pzSB->value();
-    
+
     n.x = ui->nxSB->value();
     n.y = ui->nySB->value();
     n.z = ui->nzSB->value();
-    
+
     shape1Selected = ui->shape1Combo->currentIndex();
     shape2Selected = ui->shape2Combo->currentIndex();
     shapeSelected  = ui->shapeCombo->currentIndex();
-    
+
     lastMode = ui->TabView->currentIndex();
 }
 
--- TIGLViewer/src/TIGLViewerShapeIntersectionDialog.h
+++ TIGLViewer/src/TIGLViewerShapeIntersectionDialog.h
@@ -1,3 +1,21 @@
+/*
+* Copyright (C) 2007-2013 German Aerospace Center (DLR/SC)
+*
+* Created: 2014-03-18 Martin Siggel <martin.siggel@dlr.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #ifndef TIGLVIEWERSHAPEINTERSECTIONDIALOG_H
 #define TIGLVIEWERSHAPEINTERSECTIONDIALOG_H
 
@@ -5,18 +23,20 @@
 
 #include "CTiglPoint.h"
 
-namespace Ui {
-class TIGLViewerShapeIntersectionDialog;
+namespace Ui
+{
+    class TIGLViewerShapeIntersectionDialog;
 }
 
-namespace tigl {
+namespace tigl
+{
     class CTiglUIDManager;
 }
 
 class TIGLViewerShapeIntersectionDialog : public QDialog
 {
     Q_OBJECT
-    
+
 public:
     explicit TIGLViewerShapeIntersectionDialog(tigl::CTiglUIDManager&, QWidget *parent = 0);
     ~TIGLViewerShapeIntersectionDialog();
@@ -24,11 +44,11 @@ public:
     // Returns 0 if shape/shape intersection is selected
     // Returns 1 if shape/plane intersection is selected
     int GetMode();
-    
+
     // shape - shape
     QString GetShape1UID();
     QString GetShape2UID();
-    
+
     // shape - plane
     QString GetShapeUID();
     tigl::CTiglPoint GetPoint();
@@ -38,10 +58,9 @@ private slots:
     void OnItemChanged();
 
 private:
-    
     Ui::TIGLViewerShapeIntersectionDialog *ui;
     tigl::CTiglUIDManager& uidManager;
-    
+
     static tigl::CTiglPoint p;
     static tigl::CTiglPoint n;
     static int shape1Selected;
