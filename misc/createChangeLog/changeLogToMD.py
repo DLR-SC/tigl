d1b04404335b4450d51aca9803e64de6091172dd
Tobias Stollenwerk <tobias.stollenwerk@dlr.de>
2014-04-16 10:26:05 +0200

    Included changeLog in documentation

---
+++ misc/createChangeLog/changeLogToMD.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Convert TIGL change log to valid restructured text format')
+    parser.add_argument('-i', '--input', default='ChangeLog', help='TIGL change log file')
+    parser.add_argument('-o', '--output', default='ChangeLog.rst', help='TIGL change log RST file')
+    args = parser.parse_args()
+
+    f = open(args.input,  'r')
+    lines = f.readlines()
+    f.close()
+
+    # convert to MD 
+    f = open(args.output,  'w')
+    f.write("Latest Changes\n")
+    f.write("==============\n")
+    f.write("\n")
+    for line in lines:
+        l=line.replace("    ", " ")
+        f.write(l)
+        if line.startswith('    - '):
+            f.write('\n')
+    f.close()
+
+if __name__ == "__main__":
+    main()
