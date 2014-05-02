d6bba839867fe2fd7cdef4bab752922a3b5c79dc
Mahmoud Aly <mahmoud.aly@dlr.de>
2014-05-02 09:47:55 +0200

    - Added fit to screen description - Changed TiglApp version - Changed copyright text

--- TiGLViewerAndroid/src/de/dlr/sc/tiglviewer/android/TiglViewerActivity.java
+++ TiGLViewerAndroid/src/de/dlr/sc/tiglviewer/android/TiglViewerActivity.java
@@ -473,15 +473,8 @@ public class TiglViewerActivity extends ActionBarActivity implements OnNavigatio
 					   "\n" +
 					   "Visit the TiGL project page at http://software.dlr.de/p/tigl");
 			Linkify.addLinks(tv, Linkify.WEB_URLS);
-            try {
-                tv = (TextView) findViewById(R.id.tigl_version);
-            	String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-    			tv.setText("v" + versionName);
-            }
-            catch (PackageManager.NameNotFoundException e) {
-            	// don't add version info
-            }
-
+            tv = (TextView) findViewById(R.id.tigl_version);
+            tv.setText("v1.0");
 		}
 	}
 	
