9fb52812b1f7f09ffc85205180e29c8065584c0e
Martin Siggel <martin.siggel@dlr.de>
2014-07-02 17:52:12 +0200

    Added back face sewing to prevent problems with bops

--- src/geometry/CTiglMakeLoft.cpp
+++ src/geometry/CTiglMakeLoft.cpp
@@ -173,7 +173,7 @@ void CTiglMakeLoft::makeLoftWithGuides()
     
     MakePatches SurfMaker(cguid, cprof);
     // Don't sew yet. We do it later in solid creation
-    SurfMaker.Perform(_myTolerance, 1e-4, GeomFill_CoonsC2Style, Standard_False);
+    SurfMaker.Perform(_myTolerance, 1e-4, GeomFill_CoonsC2Style, Standard_True);
     TopoDS_Shape faces = SurfMaker.Patches();
     if (SurfMaker.GetStatus() > 0) {
         LOG(ERROR) << "Could not create loft with guide curves. " << "Error code = " << SurfMaker.GetStatus();
