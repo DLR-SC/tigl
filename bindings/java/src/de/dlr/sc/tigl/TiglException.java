3c744985b922f1db6e07f13a5870d2f3a82af056
Martin Siggel <martin.siggel@dlr.de>
2014-10-23 16:03:10 +0200

    Added license headers to java bindings

--- bindings/java/src/de/dlr/sc/tigl/TiglException.java
+++ bindings/java/src/de/dlr/sc/tigl/TiglException.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2007-2014 German Aerospace Center (DLR/SC)
+ *
+ * Created: 2014-10-23 Martin Siggel <martin.siggel@dlr.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.dlr.sc.tigl;
 
 public class TiglException extends Exception{
@@ -5,24 +23,20 @@ public class TiglException extends Exception{
 	private static final long serialVersionUID = 1L;
 	
 	private TiglReturnCode errorCode = TiglReturnCode.TIGL_SUCCESS;
-	private String methodName = "";
+	private String message = "";
 	
-	public TiglException(String method, TiglReturnCode errorcode) {
+	public TiglException(String msg, TiglReturnCode errorcode) {
 		this.errorCode = errorcode;
-		this.methodName = method;
+		this.message = msg;
 	}
 	
 	public int getErrorCode() {
 		return errorCode.getValue();
 	}
 	
-	public String getLocation() {
-		return methodName;
-	}
-	
 	@Override
 	public String getMessage() {
-		return "TiglException: " + methodName + " Error code " + errorCode.toString()  
+		return "TiglException: " + message + " Error code " + errorCode.toString()  
 				+ "(" + errorCode.getValue() + ")";
 	}
 }
