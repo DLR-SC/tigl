4e259f506876615dbd30fec3425b71a53604032d
Martin Siggel <martin.siggel@dlr.de>
2014-10-23 12:45:06 +0200

    Cleaned up java generator

--- bindings/bindings_generator/java_generator.py
+++ bindings/bindings_generator/java_generator.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 01 11:43:11 2013
+Created on Wed Oct 21 10:31:30 2014
 
 @author: Martin Siggel <martin.siggel@dlr.de>
 """
@@ -27,7 +27,7 @@ class GeneratorException(Exception):
 
 class JavaGenerator(object):
     '''
-    Generates Python code wrappers from the parsed semantics
+    Generates Java code wrappers from the parsed semantics
     '''    
     
     def __init__(self, name_prefix, libname, package):
@@ -68,74 +68,6 @@ class JavaGenerator(object):
         
         return string
         
-    def create_error_handler(self, cparser):
-        string = ''
-        indent = 4*' '
-        string += 'class %sException(Exception):\n' % self.classname
-        string += '    \'\'\' The exception encapsulates the error return code of the library and arguments that were provided for the function. \'\'\'\n'
-        string += '    def __init__(self, code, *args, **kwargs):\n'
-        string += '        Exception.__init__(self)\n'
-        string += '        self.code = code\n'
-        string += '        if "error" in kwargs:\n'
-        string += '            self.error = str(kwargs["error"])\n'
-        string += '        elif code in %s._names:\n' % cparser.returncode_str
-        string += '            self.error = %s._names[code]\n' % cparser.returncode_str
-        string += '        else:\n'
-        string += '            self.error = "UNDEFINED"\n'
-        string += '        self.args = tuple(args)\n'
-        string += '        self.kwargs = dict(kwargs)\n'
-        string += '    def __str__(self):\n'
-        string += '        return self.error + " (" + str(self.code) + ") " + str(list(self.args)) + " " + str(self.kwargs)\n'      
-        string += '\n\n'
-        
-        if cparser.returncode_str in cparser.enums:
-            successcode = '%s.%s' % (cparser.returncode_str, cparser.enums[cparser.returncode_str][0])
-        else:
-            successcode = '0'
-        string += 'def catch_error(returncode, *args, **kwargs):\n'
-        string += indent + 'if returncode != %s:\n' % successcode
-        string += indent + '    raise %sException(returncode, args, kwargs)\n' \
-            % self.classname        
-        return string
-                
-
-    def create_constructor(self):
-        indent = 4*' '
-        string = ''
-        string += indent + 'def __init__(self):\n'
-        indent +=  4*' '
-        string += indent + 'self.%s = ctypes.c_int(-1)\n\n' % self.handle_str
-               
-        string += indent + '# We only support python2.5 - 3.0 now\n'
-        string += indent + 'if sys.version_info>(3,0,0):\n'
-        string += indent + '    print("Python3 not supported in %sWrapper.")\n'\
-            % self.prefix
-        string += indent + '    sys.exit()\n'
-        string += indent + 'elif sys.version_info<(2,5,0):\n'
-        string += indent + '    print("At least python 2.5 is needed from %sWrapper.")\n\n' \
-            % self.prefix
-        
-        string += indent + 'if sys.platform == \'win32\':\n'
-        string += indent + '    self.lib = ctypes.cdll.%s\n' % self.libname
-        string += indent + 'elif sys.platform == \'darwin\':\n'
-        string += indent + '    self.lib = ctypes.CDLL("lib%s.dylib")\n' % self.libname
-        string += indent + 'else:\n'
-        string += indent + '    self.lib = ctypes.CDLL("lib%s.so")\n' % self.libname
-        
-        return string
-        
-    def create_destructor(self):
-        if not self.closefunction or self.closefunction == '':
-            return ' '
-        
-        indent = 4*' '
-        string = indent + 'def __del__(self):\n'
-        indent += indent
-        string += indent + 'if hasattr(self, "lib"):\n'
-        string += indent + '    if self.lib != None:\n'
-        string += indent + '        self.%s()\n' % self.closefunction
-        string += indent + '        self.lib = None\n'
-        return string
 
     def write_enums(self, cparser, directory):
         for enumname, values in cparser.enums.iteritems():
@@ -188,45 +120,6 @@ class JavaGenerator(object):
         
         return string
     
-    def create_header(self, fun_dec, indention_depth):
-        '''Creates the method header as "def myFunc(self, myvalue):"'''
-        
-        string = ''
-        indent = 4*indention_depth*' '
-        
-        # generate simplified method name, e.g.: tixiGetValue -> getValue
-        name = fun_dec.method_name
-        if name in self.aliases:
-            name = self.aliases[name]
-        else:
-            if name.startswith(self.prefix):
-                name = name[len(self.prefix)].lower() + name[len(self.prefix)+1:]
-        
-        # position of the handle variable
-        handle_index = -1
-        
-        num_inargs  = 0
-        num_outargs = 0
-        
-        # create method header
-        string += indent + 'def %s(self' % name
-        for index, arg in enumerate(fun_dec.arguments):
-            if arg.is_handle and fun_dec.uses_handle:
-                # we dont use the handle as an function argument
-                handle_index = index
-            elif not arg.is_outarg:
-                string += ', %s' % arg.name
-                num_inargs += 1
-            elif arg.is_outarg:
-                num_outargs += 1
-                
-        for index, arg in enumerate(fun_dec.arguments):
-            # create aditional size argument for manually allocated arrays
-            if arg.arrayinfos['is_array'] and arg.is_outarg and not arg.arrayinfos['autoalloc'] and len(arg.arrayinfos['arraysizes']) == 0:
-                string += ', %s_len' % arg.name
-            
-        string += '):\n'
-        return (string, num_inargs, num_outargs, handle_index)
     
 
     def get_type(self, arg, fun_dec):
@@ -243,7 +136,7 @@ class JavaGenerator(object):
         elif not arg.arrayinfos['is_array']:
             return arg.type
         else:
-            raise GeneratorException('Cannot create python to c conversion ' +
+            raise GeneratorException('Cannot create Java to c conversion ' +
                  'for output argument "%s" in %s' % (arg.name, fun_dec.method_name), arg )
 
     
