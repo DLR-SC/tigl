427364129fe001322e7c99c4f60b18f0a3a75d42
dixxi <dixxi1@gmx.net>
2016-06-08 18:53:27 +0200

    Revert most parts of commit 9dd83425d8a8c5ff2c0cdbea4c48cb30e4090f92 "Moved some line related functionality from WingComponentSegment into own class"

--- src/wing/CCPACSWingCSStructure.h
+++ src/wing/CCPACSWingCSStructure.h
@@ -26,7 +26,7 @@
 namespace tigl
 {
 
-class CCPACSWingCSStructure // shouldn't his be CCPACSWingComponentSegmentStructure
+class CCPACSWingCSStructure
 {
 public:
     TIGL_EXPORT CCPACSWingCSStructure();
