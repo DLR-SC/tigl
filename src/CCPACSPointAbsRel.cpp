3b22acf34775a2b2e986d65ea62b7f2e6aa759f2
dixxi <dixxi1@gmx.net>
2016-07-21 13:25:11 +0200

    added missing Point and TranslationType types

---
+++ src/CCPACSPointAbsRel.cpp
@@ -0,0 +1,26 @@
+#include "CCPACSPointAbsRel.h"
+
+namespace tigl {
+	CTiglPoint CCPACSPointAbsRel::AsPoint() const {
+		CTiglPoint point;
+		// TODO: in case m_x, m_y ot m_z is not valid, we can set the corresponding coord to zero, thus AsPoint() never throws
+		point.x = m_x.get();
+		point.y = m_y.get();
+		point.z = m_z.get();
+		return point;
+	}
+
+	void CCPACSPointAbsRel::SetAsPoint(const CTiglPoint& point) {
+		m_x = point.x;
+		m_y = point.y;
+		m_z = point.z;
+	}
+
+	ECPACSTranslationType CCPACSPointAbsRel::GetRefType() const {
+		// TODO: CPACSGen does not yet support optional attributes with default values, remove this code and return m_ref when defaults become available
+		if (m_refType.isValid())
+			return m_refType.get();
+		else
+			return ECPACSTranslationType::ABS_LOCAL;
+	}
+}
