608d997d98c2f089737e1c0f9e1f69bb0aa24d7d
Martin Siggel <martin.siggel@dlr.de>
2017-08-22 16:05:06 +0200

    Make symlink to latest nightly

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == osx && -n "$FTP_PASSWORD" ]]; then
-    sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no *.dmg $FTP_USER@frs.sourceforge.net:/home/frs/project/tigl/Nightlies-macOS
+    files=( "*.dmg" )
+    PACKAGE="${files[0]}"
+    ln -s $PACKAGE TiGL-LatestNightly-Darwin.dmg
+    sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no $PACKAGE TiGL-LatestNightly-Darwin.dmg $FTP_USER@frs.sourceforge.net:/home/frs/project/tigl/Nightlies-macOS
 fi
