0d23ff83c6e366cc11cf5ed97db67d4543bcb2d9
Martin Siggel <martin.siggel@dlr.de>
2017-08-24 23:13:59 +0200

    Deploy macOS nightly builds on travis

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == osx && -n "$FTP_PASSWORD" ]]; then
+# only deploy nightly builds, not regular builds 
+if [[ "$TRAVIS_OS_NAME" == osx && -n "$FTP_PASSWORD" && "$TIGL_NIGHTLY" == "ON" ]]; then
     files=( "*.dmg" )
     PACKAGE="${files[0]}"
-    ln -s $PACKAGE TiGL-LatestNightly-Darwin.dmg
-    sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no $PACKAGE TiGL-LatestNightly-Darwin.dmg $FTP_USER@frs.sourceforge.net:/home/frs/project/tigl/Nightlies-macOS
+    ln -s $PACKAGE TiGL-Nightly-Latest-Darwin.dmg
+    sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no $PACKAGE TiGL-Nightly-Latest-Darwin.dmg $FTP_USER@frs.sourceforge.net:/home/frs/project/tigl/Nightlies-macOS
 fi
