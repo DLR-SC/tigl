b279860e395c3f2754856a9647526010d88fa154
Martin Siggel <martin.siggel@dlr.de>
2017-09-03 20:49:04 +0200

    Build and deploy documentation on linux

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -7,3 +7,8 @@ if [[ "$TRAVIS_OS_NAME" == osx && -n "$FTP_PASSWORD" && "$TIGL_NIGHTLY" == "ON" 
     ln -s $PACKAGE TiGL-Nightly-Latest-Darwin.dmg
     sshpass -p $FTP_PASSWORD scp -o StrictHostKeyChecking=no $PACKAGE TiGL-Nightly-Latest-Darwin.dmg $FTP_USER@frs.sourceforge.net:/home/frs/project/tigl/Nightlies-macOS
 fi
+
+# only deploy documentation on release builds
+if [[ "$TRAVIS_OS_NAME" == linux && -n "$FTP_PASSWORD"  && -n "$TRAVIS_TAG" ]]; then
+    sshpass -p $FTP_PASSWORD scp -r -o StrictHostKeyChecking=no install/share/doc/tigl3/html/* $FTP_USER@frs.sourceforge.net:/home/project-web/tigl/htdocs/Doc
+fi
