2015838b85bbd52c7bb0fb0a8116e4574adb1c71
Martin Siggel <martin.siggel@dlr.de>
2017-12-25 16:23:33 +0100

    Fixed build on old GCCs
    This mainly adds missing template parameters.

--- src/common/CreateIfNotExists.h
+++ src/common/CreateIfNotExists.h
@@ -5,7 +5,7 @@ namespace tigl
 {
     // define CreateIfNotExistsTag as a type which's values are compile-time constants
     struct CreateIfNotExistsTag {};
-    static const CreateIfNotExistsTag CreateIfNotExists;
+    static const CreateIfNotExistsTag CreateIfNotExists = CreateIfNotExistsTag();
 }
 
 #endif
