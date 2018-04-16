3090c7bb0d87f3e3b64c601367965c640c162807
bernhardmgruber <bernhardmgruber@gmail.com>
2018-04-16 16:27:45 +0200

    removed svn $Id$ and $Revision$ comments

--- tests/unittests/tiglWingSymmetry.cpp
+++ tests/unittests/tiglWingSymmetry.cpp
@@ -2,10 +2,6 @@
 * Copyright (C) 2007-2013 German Aerospace Center (DLR/SC)
 *
 * Created: 2013-05-13 Markus Litz <Markus.Litz@dlr.de>
-* Changed: $Id$ 
-*
-* Version: $Revision$
-*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
