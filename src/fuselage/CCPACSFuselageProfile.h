ae0856d7c1dfe3646c8d71356b03f4e22085b747
bernhardmgruber <bernhardmgruber@gmail.com>
2018-04-17 10:53:59 +0200

    Removed further svn tags in code

--- src/fuselage/CCPACSFuselageProfile.h
+++ src/fuselage/CCPACSFuselageProfile.h
@@ -3,8 +3,6 @@
 *
 * Created: 2010-08-13 Markus Litz <Markus.Litz@dlr.de>
 *
-* Version: $Revision$
-*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
