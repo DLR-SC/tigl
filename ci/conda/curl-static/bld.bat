e739051960fd682a7738a0b1c9c046fc2633f341
Martin Siggel <martin.siggel@dlr.de>
2018-06-29 11:51:27 +0200

    Fixed conda build issues
    I took the advices from conda-forge:
    https://github.com/conda-forge/staged-recipes/wiki/VC-features

--- ci/conda/curl-static/bld.bat
+++ ci/conda/curl-static/bld.bat
@@ -5,8 +5,8 @@ if errorlevel 1 exit 1
 
 REM Install step
 if %ARCH%==32 ( 
-  xcopy /e ..\builds\libcurl-vc-x86-release-static-ipv6-sspi-winssl "%LIBRARY_PREFIX%"
+  xcopy /e ..\builds\libcurl-vc%VS_MAJOR%-x86-release-static-ipv6-sspi-winssl "%LIBRARY_PREFIX%"
 ) else (
-  xcopy /e ..\builds\libcurl-vc-x64-release-static-ipv6-sspi-winssl "%LIBRARY_PREFIX%"
+  xcopy /e ..\builds\libcurl-vc%VS_MAJOR%-x64-release-static-ipv6-sspi-winssl "%LIBRARY_PREFIX%"
 )
 if errorlevel 1 exit 1
