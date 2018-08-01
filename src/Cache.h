3a13b93492229996ddcc1c74aeea1544ac010bac
bernhardmgruber <bernhardmgruber@gmail.com>
2018-08-01 12:01:38 +0200

    extended Cache by writeAccess() allowing guarded, direct cache access

--- src/Cache.h
+++ src/Cache.h
@@ -24,6 +24,28 @@
 
 namespace tigl
 {
+    template <typename T>
+    class ScopeLockedReference {
+    public:
+        ScopeLockedReference(T& reference, boost::mutex& mutex)
+            : m_reference(reference), m_mutex(mutex)
+        {
+        }
+
+        ~ScopeLockedReference() {
+            m_mutex.unlock();
+        }
+
+        ScopeLockedReference(ScopeLockedReference&&) = default;
+
+        T& operator*() const { return m_reference; }
+        T* operator->() const { return &m_reference; }
+
+    private:
+        T& m_reference;
+        boost::mutex& m_mutex;
+    };
+
     template <typename CacheStruct, typename CpacsClass>
     class Cache
     {
@@ -33,6 +55,16 @@ namespace tigl
         Cache(CpacsClass& instance, BuildFunc buildFunc)
             : m_instance(instance), m_buildFunc(buildFunc)
         {
+        }
+
+        // constructs the cache and gives direct access to it
+        // the access is guarded for the lifetime of the returned ScopeLockedReference
+        // prefer to rely on the build function for updating the cache
+        ScopeLockedReference<CacheStruct> writeAccess() {
+            boost::unique_lock<boost::mutex> lock(m_mutex);
+            if (!m_cache)
+                m_cache.emplace();
+            return ScopeLockedReference<CacheStruct>(*m_cache, *lock.release());
         }
 
         const CacheStruct& value() const
