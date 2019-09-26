8d0b4949e1cb50bbec2b08749627744f273fcf92
Martin Siggel <martin.siggel@dlr.de>
2019-09-26 23:31:42 +0200

    Fixed some macOS related issues

--- TIGLViewer/src/TIGLViewerApp.cpp
+++ TIGLViewer/src/TIGLViewerApp.cpp
@@ -50,6 +50,17 @@ void showHelp(QString appName)
 TIGLViewerApp::TIGLViewerApp(int& argc, char* argv[])
     : QApplication(argc, argv)
 {
+
+#if defined __linux__
+    // we need to set us locale as we use "." for decimal point
+    qputenv("LC_NUMERIC", "C");
+    setlocale(LC_NUMERIC, "C");
+#elif defined __APPLE__
+    setlocale(LC_NUMERIC, "C");
+#endif
+
+    mainwindow.reset(new TIGLViewerWindow);
+
     for (int iarg = 0; iarg < argc; ++iarg) {
         args.append(argv[iarg]);
     }
@@ -133,17 +144,17 @@ int TIGLViewerApp::parseArguments(QStringList argList)
 void TIGLViewerApp::onWindowInitalized()
 {
     if (!config.controlFile.isEmpty()){
-        mainwindow.setInitialControlFile(config.controlFile);
+        mainwindow->setInitialControlFile(config.controlFile);
     }
     
     // if a filename is given, open the configuration
     if (!config.initialFilename.isEmpty()) {
-        mainwindow.openFile(config.initialFilename);
+        mainwindow->openFile(config.initialFilename);
     }
     
     // if a script is given
     if (!config.initialScript.isEmpty()) {
-        mainwindow.openScript(config.initialScript);
+        mainwindow->openScript(config.initialScript);
     }
 }
 
@@ -155,11 +166,11 @@ int TIGLViewerApp::run()
         return retval;
     }
 
-    connect(&mainwindow, SIGNAL(windowInitialized()), this, SLOT(onWindowInitalized()));
+    connect(mainwindow.get(), SIGNAL(windowInitialized()), this, SLOT(onWindowInitalized()));
     if (!config.windowTitle.isEmpty()) {
-        mainwindow.setTiglWindowTitle(config.windowTitle, true);
+        mainwindow->setTiglWindowTitle(config.windowTitle, true);
     }
-    mainwindow.show();
+    mainwindow->show();
     
     retval = exec();
     return retval;
