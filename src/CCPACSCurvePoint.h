bae959701be0cb19b23952faf0619cfa69c07cba
Martin Siggel <martin.siggel@dlr.de>
2021-04-22 23:14:59 +0200

    Fixed compile issues

--- src/CCPACSCurvePoint.h
+++ src/CCPACSCurvePoint.h
@@ -7,8 +7,8 @@ namespace tigl
 class CCPACSCurvePoint : public generated::CPACSCurvePoint
 {
 public:
-    TIGL_EXPORT CCPACSCurvePoint(CCPACSWingRibExplicitPositioning* parent, CTiglUIDManager* uidMgr);
-    TIGL_EXPORT CCPACSCurvePoint(CCPACSWingRibsPositioning* parent, CTiglUIDManager* uidMgr);
+    TIGL_EXPORT CCPACSCurvePoint(CCPACSWingRibExplicitPositioning* parent);
+    TIGL_EXPORT CCPACSCurvePoint(CCPACSWingRibsPositioning* parent);
 
     TIGL_EXPORT void SetEta(const double& value) override;
     TIGL_EXPORT void SetReferenceUID(const std::string& value) override;
