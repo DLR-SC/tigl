0439b84a47a32f55a6f7b97c96cff74e008a5b5d
Martin Siggel <martin.siggel@dlr.de>
2021-06-17 15:36:15 +0200

    Updated download script

--- misc/get_tigl_tixi.sh
+++ misc/get_tigl_tixi.sh
@@ -32,29 +32,26 @@ ARCH=NO_ARCH
 LIBDIR=NO_DIR
 PACK_TYPE=NO_TYPE
 
+TIGL3VER="3.2"
+TIXIVER="3.1"
+
 read_dom () {
     local IFS=\>
     read -d \< ENTITY CONTENT
 }
 
 function printUsage {
-    echo "usage: get_tigl.sh <distro> <arch> <version>(default=3)"
+    echo "usage: get_tigl.sh <distro> <version>(default=3)"
     echo
     echo "Valid distributions:"
-    echo "    SLE_12_SP2          Suse Linux Enterprise 12 SP2"
     echo "    SLE_12_SP3          Suse Linux Enterprise 12 SP3"
-    echo "    openSUSE_13.1       openSUSE 13.1"
-    echo "    openSUSE_Leap_42.3  openSUSE Leap 42.3"
+    echo "    openSUSE_Leap_15.2  openSUSE Leap 15.2"
+    # echo "    ubuntu_20.04        Ubuntu 20.04"
     echo "    ubuntu_18.04        Ubuntu 18.04"
     echo "    ubuntu_16.04        Ubuntu 16.04"
-    echo "    ubuntu_14.04        Ubuntu 14.04"
-    echo "    fedora_rawhide      Fedora Rawhide"
-    echo "    fedora_20           Fedora 30"
+    echo "    fedora_31           Fedora 31"
+    echo "    fedora_32           Fedora 32"
     echo "    rhel_7              Red Hat Enterprise Linux 7"
-    echo
-    echo "Valid architectures:"
-    echo "    x86_64        64 Bit"
-    echo "    i386          32 Bit"
     echo
     echo "Valid versions:"
     echo "    2             TiGL 2"
@@ -64,29 +61,22 @@ function printUsage {
 function checkArguments {
 
     # check number of args
-    if [ $# -lt 2 ] || [ $# -gt 3 ]
+    if [ $# -lt 1 ] || [ $# -gt 2 ]
     then
     printUsage
         exit 1
     fi
 
-    if [ $# -eq 2 ]
+    if [ $# -eq 1 ]
     then
         tmp_ver="3"
     else
-        tmp_ver=$3
+        tmp_ver=$2
     fi
 
 
     tmp_dist=$1
-    tmp_arch=$2
-    #check arch
-    if [[ $tmp_arch != i386 && $tmp_arch != x86_64 ]]; then
-    echo "Error: Unsupported architecture:" $tmp_arch
-    echo
-    printUsage
-    exit 2
-    fi
+
 
     LIBDIR=lib
 
@@ -94,97 +84,46 @@ function checkArguments {
     if [[ $tmp_dist == SLE_12_SP3 ]]; then
         DIST=SLE_12_SP3
         PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on SLED 12"
-            exit 1
-            else
-                PACK_ARCH=x86_64
-                LIBDIR=lib64
-        fi
-    elif [[ $tmp_dist == SLE_12_SP2 ]]; then
-        DIST=SLE_12_SP2
+
+        PACK_ARCH=x86_64
+        LIBDIR=lib64
+
+    elif [[ $tmp_dist == openSUSE_Leap_15.2 ]]; then
+        DIST=openSUSE_Leap_15.2
         PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on SLED 12"
-            exit 1
-            else
-                PACK_ARCH=x86_64
-                LIBDIR=lib64
-        fi
-    elif [[ $tmp_dist == openSUSE_13.1 ]]; then
-        DIST=openSUSE_13.1
-        PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            PACK_ARCH=i586
-        else
-            PACK_ARCH=x86_64
-            LIBDIR=lib64
-        fi
-    elif [[ $tmp_dist == openSUSE_Leap_42.3 ]]; then
-        DIST=openSUSE_Leap_42.3
-        PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on openSUSE Leap 42.3"
-            exit 1
-        else
-            PACK_ARCH=x86_64
-            LIBDIR=lib64
-        fi
+
+        PACK_ARCH=x86_64
+        LIBDIR=lib64
+
     elif [[ $tmp_dist == rhel_7 ]]; then
-    DIST=RHEL_7
-    PACK_TYPE=rpm
-    if [[  $tmp_arch == i386 ]]; then
-        echo "Error: x86 architecture not available on RHEL 7"
-        exit 1
-        else
-            PACK_ARCH=x86_64
-            LIBDIR=lib64
-   fi
-   elif [[ $tmp_dist == fedora_30 ]]; then
-        DIST=Fedora_30
+      DIST=RHEL_7
+      PACK_TYPE=rpm
+      PACK_ARCH=x86_64
+      LIBDIR=lib64
+   elif [[ $tmp_dist == fedora_32 ]]; then
+        DIST=Fedora_32
         PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on Fedora 30"
-            exit 1
-        else
-            PACK_ARCH=x86_64
-            LIBDIR=lib64
-        fi
-   elif [[ $tmp_dist == fedora_rawhide ]]; then
-        DIST=Fedora_Rawhide
+        PACK_ARCH=x86_64
+        LIBDIR=lib64
+
+   elif [[ $tmp_dist == fedora_31 ]]; then
+        DIST=fedora_31
         PACK_TYPE=rpm
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on Fedora Rawhide"
-            exit 1
-        else
             PACK_ARCH=x86_64
             LIBDIR=lib64
-        fi
-    elif [[ $tmp_dist == ubuntu_14.04 ]]; then
-        DIST=xUbuntu_14.04
+    elif [[ $tmp_dist == ubuntu_20.04 ]]; then
+        DIST=xUbuntu_20.04
         PACK_TYPE=deb
-        if [[  $tmp_arch == i386 ]]; then
-            PACK_ARCH=i386
-        else
-            PACK_ARCH=amd64
-        fi
+        PACK_ARCH=amd64
     elif [[ $tmp_dist == ubuntu_16.04 ]]; then
         DIST=xUbuntu_16.04
         PACK_TYPE=deb
-        if [[  $tmp_arch == i386 ]]; then
-            PACK_ARCH=i386
-        else
-            PACK_ARCH=amd64
-        fi
+
+        PACK_ARCH=amd64
     elif [[ $tmp_dist == ubuntu_18.04 ]]; then
         DIST=xUbuntu_18.04
         PACK_TYPE=deb
-        if [[  $tmp_arch == i386 ]]; then
-            echo "Error: x86 architecture not available on Ubuntu 18.04"
-            exit 1
-        else
-            PACK_ARCH=amd64
-        fi
+        PACK_ARCH=amd64
     else
     echo "Error: Unsupported distribution:" $1
     echo
@@ -199,7 +138,7 @@ function checkArguments {
     printUsage
     exit 4
     fi
-    ARCH=$tmp_arch
+    ARCH="x86_64"
     FDIST=$tmp_dist
     TIGLVER=$tmp_ver
 }
@@ -247,13 +186,13 @@ if [[ $PACK_TYPE == rpm ]]; then
     if [[ $TIGLVER -eq "2" ]]; then
         whitelist="OCE-0* libTIGL2* libTIXI2* tigl-devel* tixi-devel* tigl-viewer*"
     elif [[ $TIGLVER -eq "3" ]]; then
-        whitelist="OCE-0* libtigl3* libtixi3* tigl3-devel* tixi3-devel* tigl3-viewer*"
+        whitelist="libopencascade7-7* libtigl3-?-$TIGL3VER* libtixi3-?-$TIXIVER* tigl3-devel-$TIGL3VER* tixi3-devel-$TIXIVER* tigl3-viewer-$TIGL3VER*"
     fi
 elif [[ $PACK_TYPE == deb ]]; then
     if [[ $TIGLVER -eq "2" ]]; then
-        whitelist="liboce-foundation1* liboce-modeling1* liboce-visualization1* libtigl-dev* libtixi-dev* libtigl2* libtixi2* tigl-viewer*"
+        whitelist="liboce-foundation1* liboce-modeling1* liboce-visualization1* libtigl-dev* libtixi-dev* libtigl2* libtixi2* tigl-viewer-$TIGL3VER*"
     elif [[ $TIGLVER -eq "3" ]]; then
-        whitelist="liboce-foundation1* liboce-modeling1* liboce-visualization1* libtigl3* libtixi3* tigl3-viewer*"
+        whitelist="libocct-foundation-7* libocct-modeling-algorithms-7*  libocct-modeling-data-7* libocct-visualization-7* libocct-data-exchange-7* libtigl3*_$TIGL3VER* libtixi3*_$TIXIVER* tigl3-viewer_$TIGL3VER*"
     fi
 else
     echo "Error: unknown package type"
@@ -305,21 +244,21 @@ done
 mv usr/ $NAME
 cd $NAME
 
-if [[ $DIST != RedHat_RHEL-5 ]]; then
-  #create start script for tiglviewer
-  echo "#!/bin/bash" > tiglviewer.sh
-  echo 'CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"' >> tiglviewer.sh
-  echo 'export LD_LIBRARY_PATH=$CURDIR/'$LIBDIR/':$LD_LIBRARY_PATH' >> tiglviewer.sh
-  if [[ $PACK_TYPE == deb ]]; then
-      echo 'export LD_LIBRARY_PATH=$CURDIR/'$LIBDIR/$ARCH-linux-gnu/':$LD_LIBRARY_PATH' >> tiglviewer.sh
-  fi
-  if [[ $TIGLVER -eq "2" ]]; then
-      echo '$CURDIR/bin/TIGLViewer' >> tiglviewer.sh
-  elif [[ $TIGLVER -eq "3" ]]; then
-      echo '$CURDIR/bin/tiglviewer-3' >> tiglviewer.sh
-  fi
-  chmod +x tiglviewer.sh
+
+#create start script for tiglviewer
+echo "#!/bin/bash" > tiglviewer.sh
+echo 'CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"' >> tiglviewer.sh
+echo 'export LD_LIBRARY_PATH=$CURDIR/'$LIBDIR/':$LD_LIBRARY_PATH' >> tiglviewer.sh
+if [[ $PACK_TYPE == deb ]]; then
+    echo 'export LD_LIBRARY_PATH=$CURDIR/'$LIBDIR/$ARCH-linux-gnu/':$LD_LIBRARY_PATH' >> tiglviewer.sh
 fi
+if [[ $TIGLVER -eq "2" ]]; then
+    echo '$CURDIR/bin/TIGLViewer' >> tiglviewer.sh
+elif [[ $TIGLVER -eq "3" ]]; then
+    echo '$CURDIR/bin/tiglviewer-3' >> tiglviewer.sh
+fi
+chmod +x tiglviewer.sh
+
 
 cd ..
 
