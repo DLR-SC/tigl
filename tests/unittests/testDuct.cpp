d1694dfe35579f1455291c88aa25a5fb2e982bd6
Anton Reiswich <anton.reiswich@dlr.de>
2022-05-04 15:23:43 +0200

    extended simpletext_ducts XML for testing exclusion lists and extended test cases in testDuct.cpp

--- tests/unittests/testDuct.cpp
+++ tests/unittests/testDuct.cpp
@@ -33,6 +33,13 @@
 #include <BRepBndLib.hxx>
 #include <gp_Pnt.hxx>
 
+#include <TopoDS_Shape.hxx>
+
+#include <TopTools_IndexedMapOfShape.hxx>
+#include <TopExp.hxx>
+
+#include <TopoDS_Iterator.hxx>
+
 /******************************************************************************/
 
 class DuctSimple : public ::testing::Test
@@ -62,8 +69,6 @@ protected:
     }
 
     void SetUp() override {
-        auto& uidMgr = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetUIDManager();
-        duct = &uidMgr.ResolveObject<tigl::CCPACSDuct>("SimpleDuct");
 
     }
     void TearDown() override {
@@ -73,118 +78,322 @@ protected:
 
     static TixiDocumentHandle           tixiHandle;
     static TiglCPACSConfigurationHandle tiglHandle;
-    tigl::CCPACSDuct const* duct {nullptr};
+
+    tigl::CTiglUIDManager& uidMgr = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetUIDManager();
+
+    // ducts
+    std::vector<std::unique_ptr<tigl::CCPACSDuct>>& ductsN = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetDucts()->GetDucts();
+
+    // duct assemblies
+    std::vector<std::unique_ptr<tigl::CCPACSDuctAssembly>>& ductAssembliesN = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetDucts()->GetDuctAssemblys();
+
+    // wing
+    tigl::CCPACSWing& wing = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetWing(1);
+
+    // fuselages
+    tigl::CCPACSFuselage& simpleFuselage = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetFuselage(1);
+    tigl::CCPACSFuselage& simpleFuselage2 = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetFuselage(2);
+
+    // expected values
+    std::vector<int> expectedNumberOfDuctsInAssembly{6,1,3,3,3,3,3,3,3,3};
+    std::vector<int> expectedNumberOfFusedSolidsInAssembly{2,1,3,3,3,3,3,3,3,3};
+
+    // duct samples
+    tigl::CCPACSDuct const* ductSimpleDuct = &uidMgr.ResolveObject<tigl::CCPACSDuct>("SimpleDuct");
+    tigl::CCPACSDuct const* ductSimpleDuct2 = &uidMgr.ResolveObject<tigl::CCPACSDuct>("SimpleDuct2");
+    tigl::CCPACSDuct const* ductYDuct1 = &uidMgr.ResolveObject<tigl::CCPACSDuct>("YDuct1");
+
+    // duct assembly samples
+    tigl::CCPACSDuctAssembly const* ductAssembly1 = &uidMgr.ResolveObject<tigl::CCPACSDuctAssembly>("DuctAssembly1");
+    tigl::CCPACSDuctAssembly const* ductAssembly2 = &uidMgr.ResolveObject<tigl::CCPACSDuctAssembly>("DuctAssembly2");
+
 };
 
 TixiDocumentHandle DuctSimple::tixiHandle = 0;
 TiglCPACSConfigurationHandle DuctSimple::tiglHandle = 0;
 
-TEST_F(DuctSimple, LoftGeometry)
+TEST_F(DuctSimple, DuctLevel)
 {
-    auto shape = duct->GetLoft();
-    const TopoDS_Shape& ductShape = shape->Shape();
+    // Check number of ducts
+    EXPECT_EQ(ductsN.size(), 31);
 
-    //Check the positioning of the duct with help of its bounding box
+    // Check if ducts are solids
+    for(std::unique_ptr<tigl::CCPACSDuct>& d : ductsN)
+    {
+        auto loftD = d->GetLoft();
+        const TopoDS_Shape& shapeD = loftD->Shape();
+        EXPECT_TRUE(shapeD.ShapeType() == TopAbs_SOLID);
+    }
+
+    // Check the position of a sample duct with help of its bounding box
+    auto loftSimpleDuct = ductSimpleDuct->GetLoft();
+    const TopoDS_Shape& shapeSimpleDuct = loftSimpleDuct->Shape();
+
     Bnd_Box ductBBox;
-    BRepBndLib::AddOptimal(ductShape, ductBBox);
+    BRepBndLib::AddOptimal(shapeSimpleDuct, ductBBox);
 
-    // X positioning of duct in coordinate system
+    // X-position of sample duct in coordinate system
     const double precisionX = 1.E-5 * (ductBBox.CornerMax().X() - ductBBox.CornerMin().X());
 
-    ASSERT_NEAR(ductBBox.CornerMax().X(), 3., precisionX );
-    ASSERT_NEAR(ductBBox.CornerMin().X(), -1., precisionX );
-
-    // Y positioning of duct in coordinate system
-    const double precisionY = 1.E-5 * (ductBBox.CornerMax().Y() - ductBBox.CornerMin().Y());
-
-    ASSERT_NEAR(ductBBox.CornerMax().Y(), 0.25, precisionY );
-    ASSERT_NEAR(ductBBox.CornerMin().Y(), -0.25, precisionY );
-
-    // Z positioning of duct in coordinate system
-    const double precisionZ = 1.E-5 * (ductBBox.CornerMax().Z() - ductBBox.CornerMin().Z());
-
-    ASSERT_NEAR(ductBBox.CornerMax().Z(), 0.1, precisionZ );
-    ASSERT_NEAR(ductBBox.CornerMin().Z(), -0.1, precisionZ );
-
-    //Check for the right number of faces
-    EXPECT_EQ(GetNumberOfFaces(ductShape), 4);
-
-    //Check for the right number of edges
-    EXPECT_EQ(GetNumberOfEdges(ductShape),10);
-
-}
-
-TEST_F(DuctSimple, SanityCheck)
-{
-    auto& ducts = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetDucts();
-
-    ASSERT_TRUE(ducts);
-    EXPECT_EQ(ducts->GetDuctAssemblys().size(), 10);
-    EXPECT_EQ(duct->GetSegments().GetSegmentCount(), 2);
-    EXPECT_EQ(duct->GetSections().GetSectionCount(), 3);
-    ASSERT_TRUE(duct->GetPositionings());
-    EXPECT_EQ(duct->GetPositionings()->GetPositionings().size(), 2);
-}
-
-TEST_F(DuctSimple, FuselageDuctCutOut)
-{
-    tiglConfigurationSetWithDuctCutouts(DuctSimple::tiglHandle, TIGL_TRUE);
-    auto& fuselage_1 = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetFuselage(2);
-    auto shape = fuselage_1.GetLoft();
-    const TopoDS_Shape& cutFuselage_1_Shape = shape->Shape();
-
-    EXPECT_FALSE(IsPointInsideShape(cutFuselage_1_Shape, gp_Pnt(0.0, 0.0, 0.0)));
-    EXPECT_TRUE(IsPointInsideShape(cutFuselage_1_Shape, gp_Pnt(0.0, 0.0, 0.25)));
-    EXPECT_FALSE(IsPointInsideShape(cutFuselage_1_Shape, gp_Pnt(0.0, 0.0, 0.6)));
-    EXPECT_FALSE(IsPointInsideShape(cutFuselage_1_Shape, gp_Pnt(0.0, 0.4, -0.1)));
-    EXPECT_FALSE(IsPointInsideShape(cutFuselage_1_Shape, gp_Pnt(1.0, 0.0, 0.25)));
-
-    //Check for the right number of faces
-    EXPECT_EQ(GetNumberOfFaces(cutFuselage_1_Shape), 12);
-
-    //Check for the right number of edges
-    EXPECT_EQ(GetNumberOfEdges(cutFuselage_1_Shape),76); //not checked
-
-    //Check if the bounding box of the cut fuselage has the right dimensions
-    Bnd_Box ductBBox;
-    BRepBndLib::AddOptimal(cutFuselage_1_Shape, ductBBox);
-
-    // X positioning of cut fuselage in coordinate system
-    const double precisionX = 1.E-5 * (ductBBox.CornerMax().X() - ductBBox.CornerMin().X());
-
-    ASSERT_NEAR(ductBBox.CornerMax().X(), 0.5, precisionX );
+    ASSERT_NEAR(ductBBox.CornerMax().X(), 1.5, precisionX );
     ASSERT_NEAR(ductBBox.CornerMin().X(), -0.5, precisionX );
-/*
-    // Y positioning of cut fuselage in coordinate system
+
+    // Y-position of sample duct in coordinate system
     const double precisionY = 1.E-5 * (ductBBox.CornerMax().Y() - ductBBox.CornerMin().Y());
 
-    ASSERT_NEAR(ductBBox.CornerMax().Y(), 0.25, precisionY );
-    ASSERT_NEAR(ductBBox.CornerMin().Y(), -0.25, precisionY );
+    ASSERT_NEAR(ductBBox.CornerMax().Y(), 0.2, precisionY );
+    ASSERT_NEAR(ductBBox.CornerMin().Y(), -0.2, precisionY );
 
-    // Z positioning of duct in coordinate system
+    // Z-position of sample duct in coordinate system
     const double precisionZ = 1.E-5 * (ductBBox.CornerMax().Z() - ductBBox.CornerMin().Z());
 
     ASSERT_NEAR(ductBBox.CornerMax().Z(), 0.1, precisionZ );
     ASSERT_NEAR(ductBBox.CornerMin().Z(), -0.1, precisionZ );
-*/
 
+    // Check number of faces of sample duct
+    EXPECT_EQ(GetNumberOfFaces(shapeSimpleDuct), 4);
+
+    // Check number of edges of sample duct
+    EXPECT_EQ(GetNumberOfEdges(shapeSimpleDuct),10);
+
+    // Check number of segments of sample duct
+    EXPECT_EQ(ductSimpleDuct->GetSegments().GetSegmentCount(), 2);
+
+    // Check number of sections of sample duct
+    EXPECT_EQ(ductSimpleDuct->GetSections().GetSectionCount(), 3);
+
+    // Check number of positionings in sample duct
+    // ASSERT_TRUE(ductSimpleDuct->GetPositionings());
+    EXPECT_EQ(ductSimpleDuct->GetPositionings()->GetPositionings().size(), 2);
 
 }
 
-TEST_F(DuctSimple, WingDuctCutOut)
+TEST_F(DuctSimple, DuctAssemblyLevel)
 {
-    tiglConfigurationSetWithDuctCutouts(DuctSimple::tiglHandle, TIGL_TRUE);
-    auto& wing = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetWing(1);
-    wing.GetLoft();
+    // Check number of duct assemblies
+    EXPECT_EQ(ductAssembliesN.size(), 10);
 
-    // TODO test geometry somehow (e.g. IsPointInside?)
+    // Check number of ducts in each assembly
+    if(ductAssembliesN.size() != expectedNumberOfDuctsInAssembly.size())
+    {
+        EXPECT_TRUE(false);
+    }
+
+    else
+    {
+        for(int i = 1; i < ductAssembliesN.size(); i++)
+        {
+            EXPECT_EQ(ductAssembliesN[i]->GetDuctUIDs().GetUIDs().size(), expectedNumberOfDuctsInAssembly[i]);
+        }
+    }
+
+    // Check if duct assemblies are solids or compounds of solids
+    for(std::unique_ptr<tigl::CCPACSDuctAssembly>& dA : ductAssembliesN)
+    {
+        auto loftDA = dA->GetLoft();
+        const TopoDS_Shape& shapeDA = loftDA->Shape();
+
+        if(shapeDA.ShapeType() == TopAbs_COMPOUND)
+        {
+            for (TopoDS_Iterator anIter(shapeDA); anIter.More(); anIter.Next())
+            {
+                TopoDS_Shape shapeIT = anIter.Value();
+                EXPECT_TRUE(shapeIT.ShapeType() == TopAbs_SOLID);
+            }  
+        }
+
+        else if(shapeDA.ShapeType() == TopAbs_SOLID)
+            continue;
+
+        else
+             EXPECT_TRUE(false);
+    }
+
+    // Check number of solids in each duct assembly after fusing
+    for(int i = 1; i < ductAssembliesN.size(); i++)
+    {
+        std::unique_ptr<tigl::CCPACSDuctAssembly>& dA = ductAssembliesN[i];
+        auto loftDA = dA->GetLoft();
+        const TopoDS_Shape& shapeDA = loftDA->Shape();
+
+        if(shapeDA.ShapeType() == TopAbs_COMPOUND)
+        {
+           TopTools_IndexedMapOfShape solids;
+           TopExp::MapShapes (shapeDA, TopAbs_SOLID, solids);
+           EXPECT_EQ(solids.Extent(), expectedNumberOfFusedSolidsInAssembly[i]);
+        }
+
+        else if(shapeDA.ShapeType() == TopAbs_SOLID)
+            EXPECT_EQ(expectedNumberOfFusedSolidsInAssembly[i], 1);
+
+        else
+             EXPECT_TRUE(false);
+    }
+
+    // Check the position of a sample duct assembly with help of its bounding box
+    auto loftDuctAssembly1 = ductAssembly1->GetLoft();
+    const TopoDS_Shape& shapeDuctAssembly1 = loftDuctAssembly1->Shape();
+
+    Bnd_Box ductBBox;
+    BRepBndLib::AddOptimal(shapeDuctAssembly1, ductBBox);
+
+    // X-position of sample duct assembly in coordinate system
+    const double precisionX = 1.E-5 * (ductBBox.CornerMax().X() - ductBBox.CornerMin().X());
+
+    ASSERT_NEAR(ductBBox.CornerMax().X(), 1.65, precisionX );
+    ASSERT_NEAR(ductBBox.CornerMin().X(), -1.25, precisionX );
+
+    // Y-position of sample duct assembly in coordinate system
+    const double precisionY = 1.E-5 * (ductBBox.CornerMax().Y() - ductBBox.CornerMin().Y());
+
+    ASSERT_NEAR(ductBBox.CornerMax().Y(), 1.1, precisionY );
+    ASSERT_NEAR(ductBBox.CornerMin().Y(), -1.0, precisionY );
+
+    // Z-position of sample duct assembly in coordinate system
+    const double precisionZ = 1.E-5 * (ductBBox.CornerMax().Z() - ductBBox.CornerMin().Z());
+
+    ASSERT_NEAR(ductBBox.CornerMax().Z(), 1.5, precisionZ );
+    ASSERT_NEAR(ductBBox.CornerMin().Z(), -0.625, precisionZ );
+
+    // Check duct symmetry
+    auto loftYDuctAssembly2 = ductAssembly2->GetLoft();
+     const TopoDS_Shape& shapeDuctAssembly2 = loftYDuctAssembly2->Shape();
+
+     for (TopoDS_Iterator anIter(shapeDuctAssembly2); anIter.More(); anIter.Next())
+     {
+         TopoDS_Shape shapeIT = anIter.Value();
+
+         {
+             EXPECT_TRUE(IsPointInsideShape(shapeIT, gp_Pnt(5.0, -0.4, 0.0)));
+         }
+     }
+}
+
+// DuctCutOutLevel
+
+TEST_F(DuctSimple, WithDuctCutoutsFalse)
+{
+    // Check if flag for duct cutouts is set to false by default
+    auto loftWing = wing.GetLoft();
+    const TopoDS_Shape& shapeWing = loftWing->Shape();
+
+    auto loftSimpleFuselage = simpleFuselage.GetLoft();
+    const TopoDS_Shape& shapeSimpleFuselage = loftSimpleFuselage->Shape();
+
+    auto loftSimpleFuselage2 = simpleFuselage2.GetLoft();
+    const TopoDS_Shape& shapeSimpleFuselage2 = loftSimpleFuselage2->Shape();
+
+    // std::vector <TopoDS_Shape> shapes{shapeWing, shapeSimpleFuselage /*,shapeSimpleFuselage2*/};
+
+    // Check if flag for duct cutouts is set to false by default
+
+    EXPECT_TRUE(IsPointInsideShape(shapeWing, gp_Pnt(0.2, 1.0, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(-0.2, 0.0, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.0, 0.0, 0.0)));
+}
+
+TEST_F(DuctSimple, WithDuctCutoutsTrue)
+{
+    // Set the flag for duct cutouts to true
+    tiglConfigurationSetWithDuctCutouts(DuctSimple::tiglHandle, TIGL_TRUE);
+
+    auto loftWing = wing.GetLoft();
+    const TopoDS_Shape& shapeWing = loftWing->Shape();
+
+    auto loftSimpleFuselage = simpleFuselage.GetLoft();
+    const TopoDS_Shape& shapeSimpleFuselage = loftSimpleFuselage->Shape();
+
+    auto loftSimpleFuselage2 = simpleFuselage2.GetLoft();
+    const TopoDS_Shape& shapeSimpleFuselage2 = loftSimpleFuselage2->Shape();
+
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.2, 1.0, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(-0.2, 0.0, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.0, 0.0, 0.0)));
+
+    // Check if the wing symmetry is respected
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.2, -1.0, 0.0)));
+
+    // Check if duct symmetry is respected in the cutout
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(5.0, -0.3, 0.0)));
+
+    // Check the exclusion list functionality
+    // Assembly 3, empty exclusion list
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.9, 1.9, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.4, -0.4, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.4, 0.4, 0.0)));
+
+    // Assembly 4, excludes SimpleFuselage
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.9, 1.7, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.2, -0.4, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.2, 0.4, 0.0)));
+
+    // Assembly 5, excludes SimpleFuselage2
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.9, 1.5, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.0, -0.4, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.0, 0.4, 0.0)));
+
+    // Assembly 6, excludes Wing
+    EXPECT_TRUE(IsPointInsideShape(shapeWing, gp_Pnt(0.9, 1.3, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(1.8, -0.4, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(5.8, 0.4, 0.0)));
+
+    // Assembly 7, excludes SimpleFuselage and SimpleFuselage2
+    EXPECT_FALSE(IsPointInsideShape(shapeWing, gp_Pnt(0.7, 1.9, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.4, 0.4, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.4, -0.4, 0.0)));
+
+    // Assembly 8, excludes Wing and SimpleFuselage
+    EXPECT_TRUE(IsPointInsideShape(shapeWing, gp_Pnt(0.7, 1.7, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.2, 0.4, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.2, -0.4, 0.0)));
+
+    // Assembly 9, excludes Wing and SimpleFuselage2
+    EXPECT_TRUE(IsPointInsideShape(shapeWing, gp_Pnt(0.7, 1.5, 0.0)));
+    EXPECT_FALSE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(2.0, 0.4, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(6.0, -0.4, 0.0)));
+
+    // Assembly 10, excludes all ducts
+    EXPECT_TRUE(IsPointInsideShape(shapeWing, gp_Pnt(0.7, 1.3, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage, gp_Pnt(1.8, 0.4, 0.0)));
+    EXPECT_TRUE(IsPointInsideShape(shapeSimpleFuselage2, gp_Pnt(5.8, -0.4, 0.0)));
+
+/*
+    // Check if loft with duct coutouts is solid or a compound of solids
+    // Check number of solids at the same time
+
+    std::vector <TopoDS_Shape> shapes{shapeWing, shapeSimpleFuselage,shapeSimpleFuselage2};
+
+    int numberOfSolids = 0;
+
+    for(TopoDS_Shape& sh : shapes)
+    {
+        //auto shapetype = sh.ShapeType();
+        //if(sh.ShapeType() == TopAbs_COMPOUND)
+        {
+            for (TopoDS_Iterator anIter(sh); anIter.More(); anIter.Next())
+            {
+                TopoDS_Shape shapeIT = anIter.Value();
+                EXPECT_TRUE(shapeIT.ShapeType() == TopAbs_SOLID);
+
+                numberOfSolids += 1;
+            }
+        }
+
+        else if(sh.ShapeType() == TopAbs_SOLID)
+        {
+            numberOfSolids += 11;
+            continue;
+        }
+
+        else
+             EXPECT_TRUE(false);
+
+    }
+    EXPECT_EQ(numberOfSolids,222);
+*/
 }
 
 TEST_F(DuctSimple, tiglConfigurationSetWithDuctCutouts){
-
-    auto& wing = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetWing(1);
-    auto& fuselage1 = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetFuselage(1);
-    auto& fuselage2 = tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetFuselage(2);
 
     // test errors
     EXPECT_EQ(TIGL_NOT_FOUND, tiglConfigurationSetWithDuctCutouts(-1, TIGL_TRUE));
@@ -194,7 +403,7 @@ TEST_F(DuctSimple, tiglConfigurationSetWithDuctCutouts){
 
     // set the flag for all components
     ASSERT_EQ(TIGL_SUCCESS, tiglConfigurationSetWithDuctCutouts(DuctSimple::tiglHandle, TIGL_TRUE));
-    EXPECT_FALSE(tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetDucts()->IsEnabled());
+    EXPECT_TRUE(tigl::CCPACSConfigurationManager::GetInstance().GetConfiguration(DuctSimple::tiglHandle).GetDucts()->IsEnabled());
 }
 
 TEST_F(DuctSimple, tiglConfigurationGetWithDuctCutouts)
@@ -216,7 +425,6 @@ TEST_F(DuctSimple, tiglConfigurationGetWithDuctCutouts)
     EXPECT_TRUE(flag);
 }
 
-
 TEST_F(DuctSimple, Export)
 {
     tiglConfigurationSetWithDuctCutouts(DuctSimple::tiglHandle, TIGL_TRUE);
@@ -226,4 +434,5 @@ TEST_F(DuctSimple, Export)
     EXPECT_EQ(TIGL_SUCCESS, tiglExportIGES(DuctSimple::tiglHandle,"TestData/export/duct_simple.iges"));
     EXPECT_EQ(TIGL_SUCCESS, tiglExportFusedWingFuselageIGES(DuctSimple::tiglHandle,"TestData/export/duct_simple_fused.iges"));
 }
+
 
