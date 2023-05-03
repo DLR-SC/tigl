95d93c8f4ea84f6c7e39379a36c1ed1d6c0065ef
Ole Albers <ole.albers@dlr.de>
2023-05-03 14:07:18 +0200

    Added test for Mesher.cpp

--- tests/unittests/tiglMesher.cpp
+++ tests/unittests/tiglMesher.cpp
@@ -19,29 +19,67 @@
 #include "test.h"
 #include "Mesher.h"
 //#include <TopoDS_Shape.hxx>
-//#include <BRepPrimAPI_MakeBox.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
 #include <gmsh.h>
 
 TEST(tiglMesher, cube)
 {
-    //gmsh::model::mesh::generate();
+    // create cube and mesh to test if linking with gmsh works
+
     gmsh::initialize();
     gmsh::model::occ::addBox(0,0,0,5,5,5);
     gmsh::model::occ::synchronize();
-   // gmsh::model::getDimension();
-    // create cube and mesh to test if linking with gmsh works
+
     ASSERT_EQ(gmsh::model::getDimension(), 3);
+
+    gmsh::finalize();
 }
+
+TEST(tiglMesher, cubeTopoDS)
+{
+    // open cpacs configuration, read wing TopoDS_Shape and create mesh
+
+    Mesher m {};
+
+    BRepPrimAPI_MakeBox testBox {5,5,5};
+    TopoDS_Shape testShape = testBox.Solid();
+
+    m.import(testShape);
+    gmsh::model::occ::synchronize();
+
+    ASSERT_EQ(gmsh::model::getDimension(), 3);
+
+
+    m.set_options(3,0.1,"testcube.msh");
+    m.mesh();
+
+    //test if the maxNodeTag of the Mesh is 430 (Mesh should have 348 Nodes)
+    std::size_t  maxTag;
+    gmsh::model::mesh::getMaxNodeTag(maxTag);
+    ASSERT_EQ(maxTag, 430);
+
+    //test if the maxElementTag of the Mesh is 6483 (Mesh should have 1790 Elements)
+    std::size_t  maxTag2;
+    gmsh::model::mesh::getMaxElementTag(maxTag2);
+    ASSERT_EQ(maxTag2, 6483);
+
+    //refine the mesh and test if the new MaxElementTag is 19913
+    m.refine();
+    std::size_t  maxTag3;
+    gmsh::model::mesh::getMaxElementTag(maxTag3);
+    ASSERT_EQ(maxTag3, 19913);
+
+}
+
 
 TEST(tiglMesher, wing)
 {
 
-
-
-    // open cpacs configuration, read wing TopoDS_Shape and create mesh
+    // refine the previously meshed file
 }
 
 TEST(tigLMesher, fused)
 {
+
     // open cpacs configuration, fuse aircract and mesh far field
 }
