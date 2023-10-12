699d9c7e66c4bf7041351e1cb0368aed1bd9b25e
Ole Albers <ole.albers@dlr.de>
2023-10-12 14:12:53 +0200

    new mesher architecture

--- src/mesher/Mesher.cpp
+++ src/mesher/Mesher.cpp
@@ -7,12 +7,6 @@ namespace tigl {
 Mesher::Mesher() { gmsh::initialize(); }
 Mesher::~Mesher(){ gmsh::finalize(); }
 
-static Mesher& get_instance()
-{
-        static Mesher m;
-        return m;
-}
-
 // Options: 
 void Mesher::set_dimension(int dimension)
 {
@@ -27,6 +21,21 @@ void Mesher::set_minMeshSize(double min)
 void Mesher::set_maxMeshSize(double max)
 {
     options.maxelementsize = max;
+}
+
+void Mesher::set_minDistance(double min_dist)
+{
+    options.mindistance = min_dist;
+}
+
+void Mesher::set_maxDistance(double max_dist)
+{
+    options.maxdistance = max_dist;
+}
+
+Mesher::FieldOptions& Mesher::get_field_options(std::string const& field_name)
+{
+    return options.field_options[field_name];
 }
 
 // imports an TopoDS_Shape by using a pointer to void
@@ -54,6 +63,74 @@ void Mesher::refine() const
     gmsh::model::mesh::refine();
     
 }
+
+
+void Mesher::mesh_fields() const
+{
+    gmsh::model::mesh::field::add("Min", num + 1 );
+    gmsh::model::mesh::field::setNumbers(num + 1, "FieldsList", Fields);
+    gmsh::model::mesh::field::setAsBackgroundMesh(num + 1);
+
+    gmsh::option::setNumber("Mesh.MeshSizeExtendFromBoundary", 0);
+    gmsh::option::setNumber("Mesh.MeshSizeFromPoints", 0);
+    gmsh::option::setNumber("Mesh.MeshSizeFromCurvature", 0);
+
+   
+    
+    gmsh::model::mesh::generate(options.dim);
+}
+
+
+// is used to refine the mesh at given curves (can be extended to points or planes)
+// anderer Weg wäre meshsize (dann allerdings Vektor mit allen Punkten und der zugehörigen Mesh Size)
+void Mesher::refineEdge( std::vector<double> Edge, std::string field_name)
+{
+    
+    Fields.push_back(num + 1);
+
+    get_field_options(field_name).field_number = Fields.back() - 1;
+
+    num = num + 2;
+
+    gmsh::model::mesh::field::add("Distance", get_field_options(field_name).field_number);
+    gmsh::model::mesh::field::setNumbers(get_field_options(field_name).field_number, "CurvesList", Edge);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number, "Sampling", 100);
+
+    gmsh::model::mesh::field::add("Threshold", get_field_options(field_name).field_number + 1);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "InField", get_field_options(field_name).field_number);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "SizeMin", get_field_options(field_name).min_element_size);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "SizeMax", options.maxelementsize);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "DistMin", options.mindistance);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "DistMax", options.maxdistance);
+
+    gmsh::model::occ::synchronize();
+    
+}
+
+void Mesher::refineSurface( std::vector<double> Surface, std::string field_name)
+{
+    
+    Fields.push_back(num + 1);
+
+    get_field_options(field_name).field_number = Fields.back() - 1;
+
+    num = num + 2;
+
+    gmsh::model::mesh::field::add("Distance", get_field_options(field_name).field_number);
+    gmsh::model::mesh::field::setNumbers(get_field_options(field_name).field_number, "SurfacesList", Surface);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number, "Sampling", 100);
+
+    gmsh::model::mesh::field::add("Threshold", get_field_options(field_name).field_number + 1);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "InField", get_field_options(field_name).field_number);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "SizeMin", get_field_options(field_name).min_element_size);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "SizeMax", options.maxelementsize);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "DistMin", options.mindistance);
+    gmsh::model::mesh::field::setNumber(get_field_options(field_name).field_number + 1, "DistMax", options.maxdistance);
+
+    gmsh::model::occ::synchronize();
+    
+}
+
 
 // writes the mesh in the given format 
 void Mesher::write(std::string myName) const
--- src/mesher/Mesher.h
+++ src/mesher/Mesher.h
@@ -2,37 +2,63 @@
 #include "gmsh.h"
 #include <string>
 #include "TopoDS_Shape.hxx"
+#include <unordered_map>
+#include <map>
 
 namespace tigl {
 
 
-// Mesher is a singleton, that means there can ever be only one instance
 class Mesher 
 {
 public: 
-       struct Options
+
+    struct FieldOptions 
     {
-        int dim;
+        double min_element_size;
+        double field_number;
+    };
+    
+    struct Options
+    {
+        int dim; 
+
         double minelementsize;
         double minelementsize2;
         double minelementsize3;
         double maxelementsize;
         double mindistance;
         double maxdistance;
+
+        std::map<std::string, FieldOptions> field_options;
+
     };
 
-Mesher();
-~Mesher();
-void set_dimension(int dimension);
-void set_minMeshSize(double min);
-void set_maxMeshSize(double max);
-void mesh() const;
-void import(TopoDS_Shape shape) const;
-
-void refine() const;
-void write(std::string myName) const;
     
 
+    Mesher();
+    ~Mesher();
+    
+    FieldOptions& get_field_options(std::string const& field_name);
+
+    void set_dimension(int dimension);
+    void set_minMeshSize(double min);
+    void set_maxMeshSize(double max);
+    void set_minDistance(double min_dist);
+    void set_maxDistance(double max_dist);
+    void mesh() const;
+    void import(TopoDS_Shape shape) const;
+
+    void refine() const;
+    void write(std::string myName) const;
+
+    void refineEdge( std::vector<double> Edge, std::string field_name);
+    void refineSurface( std::vector<double> Surface, std::string field_name);
+    void mesh_fields() const;
+
+    std::vector<double> Fields;
+    double num = 1;
+
     Options options;
+    
 };
 }
