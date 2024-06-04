0e5762f06ddf217a541fa6b1a8be442c34e5a111
Kobold <meike.kobold@dlr.de>
2024-06-04 16:38:33 +0200

    updated testfile

---
+++ tests/unittests/testFuselageStandardProfileSuperellipse.cpp
@@ -0,0 +1,91 @@
+/*
+* Copyright (C) 2022 German Aerospace Center
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "CTiglMakeLoft.h"
+#include "test.h"
+#include "tigl.h"
+#include "Debugging.h"
+#include "tiglcommonfunctions.h"
+#include "BRepBuilderAPI_Transform.hxx"
+#include <BRepCheck_Analyzer.hxx>
+#include "CCPACSConfigurationManager.h"
+#include "CCPACSConfiguration.h"
+#include "CNamedShape.h"
+
+class FuselageStandardProfileSuperellipse : public ::testing::Test
+{
+protected:
+    static void SetUpTestCase()
+    {
+        // Test case on standardProfile, mixed profiles: rectangle, rectangle with rounded corners, circle, circle with kinks
+
+        const char* filename = "TestData/simpletest_standard_profile_rectangle_circle_kink.cpacs.xml";
+        ReturnCode tixiRet;
+        TiglReturnCode tiglRet;
+
+        tiglHandle = -1;
+        tixiHandle = -1;
+
+        tixiRet = tixiOpenDocument(filename, &tixiHandle);
+        ASSERT_TRUE (tixiRet == SUCCESS);
+        tiglRet = tiglOpenCPACSConfiguration(tixiHandle, "", &tiglHandle);
+        ASSERT_TRUE(tiglRet == TIGL_SUCCESS);
+
+        // Test case on standardProfile rectangle with guide curves
+
+        const char* filename1 = "TestData/simpletest_standard_profile_rectangle_circle_guides.cpacs.xml";
+        ReturnCode tixiRet1;
+        TiglReturnCode tiglRet1;
+
+        tiglHandle1 = -1;
+        tixiHandle1 = -1;
+
+        tixiRet1 = tixiOpenDocument(filename1, &tixiHandle1);
+        ASSERT_TRUE (tixiRet1 == SUCCESS);
+        tiglRet1 = tiglOpenCPACSConfiguration(tixiHandle1, "", &tiglHandle1);
+        ASSERT_TRUE(tiglRet1 == TIGL_SUCCESS);
+
+
+    }
+
+    static void TearDownTestCase()
+    {
+        ASSERT_TRUE(tiglCloseCPACSConfiguration(tiglHandle) == TIGL_SUCCESS);
+        ASSERT_TRUE(tixiCloseDocument(tixiHandle) == SUCCESS);
+        tiglHandle = -1;
+        tixiHandle = -1;
+
+        ASSERT_TRUE(tiglCloseCPACSConfiguration(tiglHandle1) == TIGL_SUCCESS);
+        ASSERT_TRUE(tixiCloseDocument(tixiHandle1) == SUCCESS);
+        tiglHandle1 = -1;
+        tixiHandle1 = -1;
+
+    }
+
+    void SetUp() override {}
+    void TearDown() override {}
+
+
+    static TixiDocumentHandle           tixiHandle;
+    static TiglCPACSConfigurationHandle tiglHandle;
+
+    static TixiDocumentHandle           tixiHandle1;
+    static TiglCPACSConfigurationHandle tiglHandle1;
+
+};
+
+
+
+
