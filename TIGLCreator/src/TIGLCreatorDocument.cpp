df11413b780628fbabd865696e42cfe8106e5fed
Ole Albers <ole.albers@dlr.de>
2025-12-05 14:57:52 +0100

    fix merge error

--- TIGLCreator/src/TIGLCreatorDocument.cpp
+++ TIGLCreator/src/TIGLCreatorDocument.cpp
@@ -757,21 +757,32 @@ void TIGLCreatorDocument::drawComponentByUID(const QString& uid)
         if (!app->getScene()->GetShapeManager().HasShapeEntry(uid.toStdString())) {
             PNamedShape loft = component.GetLoft();
 
-        if (loft) {
-            double opacity = 0;
-            bool shaded = true;
-            // By default, we display the wing without cutouts (for performance). 
-            // Therefore, it is visually better to display the flaps using a wireframe rendering by default
-            if (component.GetComponentType() == TIGL_COMPONENT_CONTROL_SURFACE_DEVICE) {
-                shaded = false;
-            }
-            app->getScene()->displayShape(loft, true, getDefaultShapeColor(), opacity, shaded);
-            if (geometricComp) {
-                PNamedShape mirroredLoft = geometricComp->GetMirroredLoft();
-                if (mirroredLoft) {
-                    app->getScene()->displayShape(mirroredLoft, true, getDefaultShapeSymmetryColor(), opacity, shaded);
+            if (loft) {
+                double opacity = 0;
+                bool shaded = true;
+                // By default, we display the wing without cutouts (for performance). 
+                // Therefore, it is visually better to display the flaps using a wireframe rendering by default
+                if (component.GetComponentType() == TIGL_COMPONENT_CONTROL_SURFACE_DEVICE) {
+                    shaded = false;
+                }
+
+                app->getScene()->displayShape(loft, true, getDefaultShapeColor(), opacity, shaded);
+
+                auto* geometricComp = dynamic_cast<tigl::CTiglAbstractGeometricComponent*>(&component);
+                if (geometricComp) {
+                    PNamedShape mirroredLoft = geometricComp->GetMirroredLoft();
+                    if (mirroredLoft) {
+                        app->getScene()->displayShape(mirroredLoft, true, getDefaultShapeSymmetryColor(), opacity, shaded);
+                    }
                 }
             }
+        }
+        else {
+            IObjectList objects = app->getScene()->GetShapeManager().GetIObjectsFromShapeName(uid.toStdString());
+            for (auto& obj : objects) {
+                app->getScene()->getContext()->Display(obj, Standard_False);
+            }
+            app->getScene()->getViewer()->Update();
         }
     }
     catch(tigl::CTiglError& err) {
