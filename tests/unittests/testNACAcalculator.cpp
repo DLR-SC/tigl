796b527f741a5b5183280face8bdc31d1955a223
Gedler, Hannah <hannah.gedler@dlr.de>
2026-01-26 15:36:42 +0100

    refactored the code for NACA4calculator, it works now

--- tests/unittests/testNACAcalculator.cpp
+++ tests/unittests/testNACAcalculator.cpp
@@ -20,6 +20,24 @@
 
 TEST(NACAcalculator, Nacacalculatortest1){
     NACAcalculator  NACA(2,2,12);
-    EXPECT_NEAR(NACA.upper_y_fct(1), 0.00125843, 1e-14);
-    EXPECT_NEAR(NACA.lower_y_fct(1), -0.00125843, 1e-14);
+    EXPECT_NEAR(NACA.upper_y_fct(1), 0.00125843, 1e-5);
+    EXPECT_NEAR(NACA.lower_y_fct(1), -0.00125843, 1e-5);
+}
+
+TEST(NACAcalculator, Nacacalculatortest2){
+    NACAcalculator NACA(0,0,12);
+    EXPECT_NEAR(NACA.upper_y_fct(0.5), 0.0529403, 1e-5);
+    EXPECT_NEAR(NACA.lower_y_fct(0.5), -0.0529403, 1e-5);
+}
+
+TEST(NACAcalculator, Nacacalculatortest3){
+    NACAcalculator NACA(0,0,9);
+    EXPECT_NEAR(NACA.upper_y_fct(0.2), 0.0430316, 1e-5);
+    EXPECT_NEAR(NACA.lower_y_fct(0.2), -0.0430316, 1e-5);
+}
+
+TEST(NACAcalculator, Nacacalculatortest4){
+    NACAcalculator NACA(6,5,9);
+    EXPECT_NEAR(NACA.upper_y_fct(1), 0.000918906, 1e-4);
+    EXPECT_NEAR(NACA.lower_y_fct(1), -0.000918906, 1e-4);
 }
