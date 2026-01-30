7035169300874ba67c43ba53024d63d00bbe8443
MarAlder <43143741+MarAlder@users.noreply.github.com>
2026-01-30 14:51:43 +0100

    initiate Python tests

---
+++ tests/unittests/test_systems.py
@@ -0,0 +1,47 @@
+#############################################################################
+# Copyright (C) 2007-2025 German Aerospace Center (DLR/SC)
+#
+# Created: 2025-30-01 Marko Alder <marko.alder@dlr.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#############################################################################
+
+import unittest
+
+from tigl3.tigl3wrapper import *
+from tixi3.tixi3wrapper import *
+from tigl3 import configuration
+
+
+class Systems(unittest.TestCase):
+
+    def setUp(self):
+        self.tixi = Tixi3()
+        self.tigl = Tigl3()
+        self.assertIsNone(self.tixi.open("TestData/simpletest-systems.cpacs.xml"))
+        self.assertIsNone(self.tigl.open(self.tixi, ""))
+
+        mgr = configuration.CCPACSConfigurationManager.get_instance()
+        self.config = mgr.get_configuration(self.tigl._handle.value)
+        uid_mgr = self.config.get_uidmanager()
+
+    def tearDown(self):
+        self.tigl.close()
+        self.tixi.close()
+
+    def test_configuration(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
