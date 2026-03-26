d552dda812eb42fd800d3cdb78669275d14b2ee7
Gedler, Hannah <hannah.gedler@dlr.de>
2026-03-26 14:09:05 +0100

    added asserts for every instance to the python tests

--- tests/unittests/TestData/test_naca4profile.py
+++ tests/unittests/TestData/test_naca4profile.py
@@ -2,6 +2,11 @@ from tixi3.tixi3wrapper import Tixi3
 from tigl3.tigl3wrapper import Tigl3
 import tigl3.configuration
 import unittest
+import os
+import sys
+from OCC.Core.TopoDS import TopoDS_Edge
+from OCC.Core.gp import gp_Vec2d
+from tigl3.configuration import NACA4Calculator
 
 
 class TestNACA(unittest.TestCase):
@@ -24,13 +29,20 @@ class TestNACA(unittest.TestCase):
         choice = self.wing_profile.get_naca_profile_choice4()
         assert isinstance(self.wing_profile.get_naca_profile_choice4(), tigl3.configuration.CPACSNacaProfile)
         upper_wire = self.wing_profile.get_upper_wire()
+        assert isinstance(upper_wire, TopoDS_Edge)
         lower_wire = self.wing_profile.get_lower_wire()
+        assert isinstance(lower_wire, TopoDS_Edge)
         trailing_edge = self.wing_profile.get_trailing_edge()
+        assert isinstance(trailing_edge, TopoDS_Edge)
 
         upper_point = self.wing_profile.get_upper_point(1.0)
+        point = upper_point.Z()
+        self.assertAlmostEqual(point, 0, delta=1e-6)
 
         calculator = tigl3.configuration.NACA4Calculator(0,0,9, 0.000945)
+        assert isinstance(calculator, NACA4Calculator)
         calculator_upper_curve = calculator.upper_curve(0.5)
-        #help(calculator_upper_curve)
+        assert isinstance(calculator_upper_curve, gp_Vec2d)
+        
 if __name__ == "__main__":
     unittest.main()
