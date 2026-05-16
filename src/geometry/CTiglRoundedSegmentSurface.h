edd2931a844a0e681aee3b9c42e54e40d75dbb7e
Meike Kobold <meike.kobold@dlr.de>
2026-05-16 12:18:29 +0200

    added new class

---
+++ src/geometry/CTiglRoundedSegmentSurface.h
@@ -0,0 +1,10 @@
+#ifndef CTIGLROUNDEDSEGMENTSURFACE_H
+#define CTIGLROUNDEDSEGMENTSURFACE_H
+
+class CTiglRoundedSegmentSurface
+{
+public:
+    CTiglRoundedSegmentSurface();
+};
+
+#endif // CTIGLROUNDEDSEGMENTSURFACE_H
