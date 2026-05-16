9844e0987e46b5353300e50ddb9deeaee3ec0763
Meike Kobold <meike.kobold@dlr.de>
2026-05-16 14:37:43 +0200

    sketch header file

--- src/geometry/CTiglRoundedSegmentSurface.h
+++ src/geometry/CTiglRoundedSegmentSurface.h
@@ -1,10 +1,34 @@
 #ifndef CTIGLROUNDEDSEGMENTSURFACE_H
 #define CTIGLROUNDEDSEGMENTSURFACE_H
 
+#include "Geom_BSplineCurve.hxx"
+#include "TopoDS_Wire.hxx"
+#include "tigl_internal.h"
+#include <vector>
+
+namespace tigl {
+
 class CTiglRoundedSegmentSurface
 {
 public:
-    CTiglRoundedSegmentSurface();
+    TIGL_EXPORT CTiglRoundedSegmentSurface(const std::vector<TopoDS_Wire>&, double inner_rounding_distance, double outer_rounding_distance);
+
+    TIGL_EXPORT TopoDS_Shape& Shape();
+
+private:
+    // Converts TopoDS_Wire → Handle(Geom_BSplineCurve)
+    TIGL_EXPORT void Perform();
+    TIGL_EXPORT void Invalidate() { _hasPerformed = false; }
+    TIGL_EXPORT void ConvertCurves();
+    TIGL_EXPORT void buildLoft();
+
+
+    const std::vector<TopoDS_Wire>& profileWires;
+    // Store as B-spline curves internally (after conversion)
+    std::vector<Handle(Geom_BSplineCurve)> m_profileCurves;
+
+
 };
 
+}
 #endif // CTIGLROUNDEDSEGMENTSURFACE_H
