e0229d761f929e05c5013a0b530dcb886f4881b2
MarAlder <43143741+MarAlder@users.noreply.github.com>
2026-05-22 11:33:50 +0200

    add/modify licence texts

--- src/geometry/CCPACSTransformationSE3.h
+++ src/geometry/CCPACSTransformationSE3.h
@@ -1,7 +1,7 @@
 /*
-* Copyright (C) 2015 German Aerospace Center (DLR/SC)
+* Copyright (C) 2007-2026 German Aerospace Center (DLR/SC)
 *
-* Created: 2015-05-27 Martin Siggel <Martin.Siggel@dlr.de>
+* Created: 2026-01-25 Marko Alder <marko.alder@dlr.de>
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
