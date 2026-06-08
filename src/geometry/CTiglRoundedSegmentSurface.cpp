3e7a7dd12e96e8e9c528b04f96927bf3cfc328a1
merakulix <meike.kobold@dlr.de>
2026-06-08 13:18:03 +0200

    calculate curves in RoundedSegment Constructor

--- src/geometry/CTiglRoundedSegmentSurface.cpp
+++ src/geometry/CTiglRoundedSegmentSurface.cpp
@@ -14,9 +14,9 @@ CTiglRoundedSegmentSurface::CTiglRoundedSegmentSurface(const  std::vector<Handle
                                                        double outer_rounding_distance, int u_degree, int v_degree):
     m_pole_matrix(1,m_profileCurves.size()+(m_profileCurves.size()-2)*6,1, (m_profileCurves[0]->NbPoles())),
     m_u_knots(1,m_profileCurves[0]->NbKnots()),
-    m_v_knots(1, m_pole_matrix.ColLength()+v_degree-5),
-    m_u_multiplicities(1, m_pole_matrix.RowLength()+u_degree+1, 1 ),
-    m_v_multiplicities(1, m_pole_matrix.ColLength()+v_degree-5, 1),
+    m_v_knots(1, m_pole_matrix.ColLength()-v_degree+1),
+    m_u_multiplicities(m_profileCurves[0]->Multiplicities()),
+    m_v_multiplicities(1, m_pole_matrix.ColLength()-v_degree+1, 1),
     m_inner_rounding_distance(inner_rounding_distance),
     m_outer_rounding_distance(outer_rounding_distance),
     m_profileCurves(m_profileCurves),
@@ -49,7 +49,7 @@ TIGL_EXPORT  Handle(Geom_BSplineSurface) CTiglRoundedSegmentSurface::Surface(){
         std::cerr<< "Column:" << col << std::endl;
         for(int i=1; i < m+1; i++){
             i_poles.SetValue(i,(m_pole_matrix.Value(i,col)));
-            std::cerr << "X:" << i_poles.Value(i).Coord().X() << " Y:" << i_poles.Value(i).Coord().Y() << " Z:" << i_poles.Value(i).Coord().Z() << std::endl;
+            std::cerr << "X:" << i_poles.Value(i).Coord().X() << "\t Y:" << i_poles.Value(i).Coord().Y() << "\t Z:" << i_poles.Value(i).Coord().Z() << std::endl;
         }
         auto spline = new Geom_BSplineCurve(i_poles, m_v_knots, m_v_multiplicities, _v_degree);
         TopoDS_Edge edge = BRepBuilderAPI_MakeEdge(spline);
@@ -60,13 +60,20 @@ TIGL_EXPORT  Handle(Geom_BSplineSurface) CTiglRoundedSegmentSurface::Surface(){
         std::cerr<< "Row:" << row << std::endl;
         for(int i=1; i < n+1; i++){
             i_poles.SetValue(i,(m_pole_matrix.Value(row,i)));
-            std::cerr << "X:" << i_poles.Value(i).Coord().X() << " Y:" << i_poles.Value(i).Coord().Y() << " Z:" << i_poles.Value(i).Coord().Z() << std::endl;
+            std::cerr << "X:" << i_poles.Value(i).Coord().X() << "\t Y:" << i_poles.Value(i).Coord().Y() << "\t Z:" << i_poles.Value(i).Coord().Z() << std::endl;
         }
         auto spline = new Geom_BSplineCurve(i_poles, m_u_knots, m_u_multiplicities, _u_degree);
         TopoDS_Edge edge = BRepBuilderAPI_MakeEdge(spline);
         tigl::dumpShape(edge,"makeLoftWing", "Row",row);
     }
+    std::cerr << "Dimensions:\nPolematrix:\nrows: " << m_pole_matrix.ColLength()
+              << "\ncol: " << m_pole_matrix.RowLength()
+              << "\n v_knot number: " << m_v_knots.Length()
+              << "\n u_knot number: " << m_u_knots.Length()
+              << "\n v_mults number: " << m_v_multiplicities.Length()
+              << "\n u_mults number: " << m_u_multiplicities.Length() <<std::endl;
     //END DEBUG DELETEME
+
 
     auto surface = new Geom_BSplineSurface(m_pole_matrix, m_u_knots, m_v_knots, m_u_multiplicities, m_v_multiplicities, _u_degree, _v_degree, false, false);
     return surface;
@@ -79,24 +86,22 @@ void CTiglRoundedSegmentSurface::Perform(){
     calculateKnotsAndMultiplicities();
 
     //initialize m_segments vector
-    for(int i = 0; i < m_profileCurves.size()-1; i++){
+    m_segments.push_back(RoundedSegment(m_profileCurves[0],
+                                            m_profileCurves[1],
+                                            0., //TODO access the right values per RoundedSegment later -> Put this line back in loop
+                                            m_outer_rounding_distance));
+    for(int i = 1; i < m_profileCurves.size()-2; i++){
         m_segments.push_back(RoundedSegment(m_profileCurves[i],
                                             m_profileCurves[i+1],
                                             m_inner_rounding_distance, //TODO access the right values per RoundedSegment
                                             m_outer_rounding_distance));
     }
-    //update each element in m_segments vector and create all dummy profiles
-    //outer segments have only inner|outer dummy curves
-    m_segments[0].insert_outer_rows(_nb_dummies);
-    m_segments[m_segments.size()-1].insert_inner_rows(_nb_dummies);
-    //inner segments have both, inner and outer dummy curves
-    if(m_segments.size()>3){
-        for(int i = 1; i< m_segments.size()-1; i++){
-            m_segments[i].insert_inner_rows(_nb_dummies);
-            m_segments[i].insert_outer_rows(_nb_dummies);
-        }
-    }
+    m_segments.push_back(RoundedSegment(m_profileCurves[m_profileCurves.size()-2],
+                                        m_profileCurves[m_profileCurves.size()-1],
+                                        m_inner_rounding_distance, //TODO access the right values per RoundedSegment later -> Put this line back in loop
+                                        0.));
 
+    //Write poles in pole matrix
     int row=1; //counter for inserted rows
     std::cerr << "col:" << m_pole_matrix.NbColumns() << " rows: " << m_pole_matrix.NbRows() << std::endl;
     for(RoundedSegment seg: m_segments){
@@ -124,7 +129,8 @@ void CTiglRoundedSegmentSurface::Perform(){
 
 void CTiglRoundedSegmentSurface::calculateKnotsAndMultiplicities(){
     //create knots in v-direction
-    auto nb_v_knots = m_pole_matrix.ColLength()+_v_degree-5;
+    //auto nb_v_knots = m_pole_matrix.ColLength()+_v_degree-5;
+    auto nb_v_knots = m_pole_matrix.ColLength()-_v_degree+1;
     std::vector<double> tmp_v = LinspaceWithBreaks(0., 1., nb_v_knots,{});
     for(int i=1;i<m_v_knots.Length()+1; i++){
         m_v_knots.SetValue(i,tmp_v[i-1]);
@@ -132,7 +138,7 @@ void CTiglRoundedSegmentSurface::calculateKnotsAndMultiplicities(){
     //create multiplicities in v-direction
     m_v_multiplicities.SetValue(1,4);
     m_v_multiplicities.SetValue((nb_v_knots),4);
-    //initialize knots in u-direction
+    //initialize knots in u-direction(copy from originial 1. profile curve fits all)
     m_u_knots = m_profileCurves[0]->Knots();
     m_u_multiplicities = m_profileCurves[0]->Multiplicities();
 
