8189fd290542f4dd22c3a5d25f0c3f5b9d334a8c
MarAlder <43143741+MarAlder@users.noreply.github.com>
2026-06-09 16:20:39 +0200

    use CheckExceptionMessage from testUtils.h

--- tests/unittests/tiglDecks.cpp
+++ tests/unittests/tiglDecks.cpp
@@ -18,6 +18,7 @@
 
 #include "test.h"
 #include "tigl.h"
+#include "testUtils.h"
 
 #include <boost/optional/optional_io.hpp>
 
@@ -324,20 +325,6 @@ protected:
 
 TixiDocumentHandle InvalidDecks::tixiHandle           = 0;
 TiglCPACSConfigurationHandle InvalidDecks::tiglHandle = 0;
-
-static void CheckExceptionMessage(const std::function<void()>& func, const char* expectedMessage)
-{
-    try {
-        func();
-        FAIL() << "Expected tigl::CTiglError but no exception was thrown.";
-    }
-    catch (const tigl::CTiglError& e) {
-        EXPECT_STREQ(e.what(), expectedMessage);
-    }
-    catch (...) {
-        FAIL() << "Expected tigl::CTiglError but a different exception was thrown.";
-    }
-}
 
 TEST_F(InvalidDecks, UnsupportedReferences)
 {
