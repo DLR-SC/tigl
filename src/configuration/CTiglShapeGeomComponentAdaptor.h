f0dfbc4f0cfe59a0a428c6596772f0332df5b27c
Jan Kleinert <jan.kleinert@dlr.de>
2026-07-02 11:10:52 +0200

    remove override method that does not really override

--- src/configuration/CTiglShapeGeomComponentAdaptor.h
+++ src/configuration/CTiglShapeGeomComponentAdaptor.h
@@ -78,12 +78,6 @@ public:
         return PNamedShape();
     }
 
-    // Default trimmed loft implementation falls back to the regular loft
-    PNamedShape GetTrimmedLoft() const override
-    {
-        return GetLoft();
-    }
-
     TiglGeometricComponentType GetComponentType() const override
     {
         return TIGL_COMPONENT_OTHER;
